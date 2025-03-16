804fccbdd07b8b630b385e5b2ef02730be89c561
GrizlyBear <michail.zevahin@gmail.com>
2025-03-16 14:15:15 +0300

    Вывод информации о пользовательских типах

---
+++ querries/meta_data.sql
@@ -0,0 +1,15 @@
+SELECT 
+    n.nspname AS "Schema",
+    p.typname AS "Type Name",
+    pg_catalog.format_type(p.typbasetype, NULL) AS "Base Type",
+    (SELECT array_agg(e.enumlabel) 
+     FROM pg_enum e 
+     WHERE e.enumtypid = p.oid) AS "Enum Values"
+FROM 
+    pg_catalog.pg_type p
+JOIN 
+    pg_catalog.pg_namespace n ON n.oid = p.typnamespace
+WHERE 
+    p.typtype = 'e' -- 'e' для ENUM
+ORDER BY 
+    "Schema", "Type Name";
