9626acf1a0548532cbe59e9e9da611157e4fb9c4
GrizlyBear <michail.zevahin@gmail.com>
2025-03-16 14:15:46 +0300

    Дописан скрипт создания таблиц БД

--- querries/create_tables.sql
+++ querries/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE IF NOT EXISTS Client (
 	ID SERIAL PRIMARY KEY,
 	mail VARCHAR(255)
 );
@@ -8,16 +8,83 @@ CREATE TABLE IF NOT EXISTS Duty (
 	mail VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS Trajectory (
-	ID SERIAL PRIMARY KEY,
-	city VARCHAR(255),
-	title VARCHAR(255)
-	svd_content TEXT
-);
-
 CREATE TABLE IF NOT EXISTS Chart (
 	ID SERIAL PRIMARY KEY,
 	city VARCHAR(255),
-	title VARCHAR(255)
-	svd_content TEXT
+	title VARCHAR(255),
+	svg_content TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Branch (
+	ID SERIAL PRIMARY KEY,
+	chartID INT,
+	title VARCHAR(255),
+	color INT, -- HEX число [0, 16777215]
+	access_type "accesstype",
+	FOREIGN KEY (chartID) REFERENCES Chart (ID)
+);
+
+CREATE TABLE IF NOT EXISTS Station (
+	ID SERIAL PRIMARY KEY,
+	dutyID INT,
+	title VARCHAR(255),
+	occupancy INT, -- [0, 10]
+	access_type "accesstype",
+	open_time TIMESTAMP,
+	close_time TIMESTAMP,
+	FOREIGN KEY (dutyID) REFERENCES Duty (ID)
+);
+
+CREATE TABLE IF NOT EXISTS Transition (
+	ID SERIAL PRIMARY KEY,
+	dutyID INT,
+	occupancy INT, -- [0, 10]
+	access_type "accesstype",
+	open_time TIMESTAMP,
+	close_time TIMESTAMP,
+	FOREIGN KEY (dutyID) REFERENCES Duty (ID)
+);
+
+CREATE TABLE IF NOT EXISTS BranchStation (
+	ID SERIAL PRIMARY KEY,
+	branchID INT,
+	stationID INT,
+	FOREIGN KEY (branchID) REFERENCES Branch (ID),
+	FOREIGN KEY (stationID) REFERENCES Station (ID)
+);
+
+CREATE TABLE IF NOT EXISTS StationStation (
+	ID SERIAL PRIMARY KEY,
+	fromID INT,
+	toID INT,
+	duration INT, -- в секундах
+	FOREIGN KEY (fromID) REFERENCES Station (ID),
+	FOREIGN KEY (toID) REFERENCES Station (ID)
+);
+
+CREATE TABLE IF NOT EXISTS StationTransition (
+	ID SERIAL PRIMARY KEY,
+	stationID INT,
+	transitionID INT,
+	FOREIGN KEY (stationID) REFERENCES Station (ID),
+	FOREIGN KEY (transitionID) REFERENCES Transition (ID)
+);
+
+CREATE TABLE IF NOT EXISTS Trajectory (
+	ID SERIAL PRIMARY KEY,
+	clientID INT,
+	chartID INT,
+	title VARCHAR(255),
+	init_date TIMESTAMP,
+	FOREIGN KEY (clientID) REFERENCES Client (ID),
+	FOREIGN KEY (chartID) REFERENCES Chart (ID)
+);
+
+CREATE TABLE IF NOT EXISTS TrajectoryLead (
+	ID SERIAL PRIMARY KEY,
+	TrajectoryID INT,
+	TrajectoryChainID INT,
+	chain_type "chaintype",
+	step_nomer INT,
+	FOREIGN KEY (TrajectoryID) REFERENCES Trajectory (ID)
 );
