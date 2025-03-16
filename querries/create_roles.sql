f3fefd35a52e17814117e179986138c2dfa84dc5
GrizlyBear <michail.zevahin@gmail.com>
2025-03-16 15:18:11 +0300

    Обновлено создание ролей БД

--- querries/create_roles.sql
+++ querries/create_roles.sql
@@ -1,5 +1,20 @@
-CREATE ROLE IF NOT EXISTS WITH LOGIN PASSWORD 'unsigned_user';
-CREATE ROLE IF NOT EXISTS WITH LOGIN PASSWORD 'signed_user';
-CREATE ROLE IF NOT EXISTS WITH LOGIN PASSWORD 'duty_user';
-CREATE ROLE IF NOT EXISTS WITH LOGIN PASSWORD 'admin_user';
-CREATE ROLE IF NOT EXISTS WITH LOGIN PASSWORD 'dev_user';
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_roles WHERE rolname = 'unsigned_client'
+    ) THEN
+        CREATE ROLE unsigned_client WITH LOGIN PASSWORD 'unsigned_client';
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_roles WHERE rolname = 'signed_client'
+    ) THEN
+        CREATE ROLE signed_client WITH LOGIN PASSWORD 'signed_client';
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_roles WHERE rolname = 'duty'
+    ) THEN
+        CREATE ROLE duty WITH LOGIN PASSWORD 'duty';
+    END IF;
+END $$;
