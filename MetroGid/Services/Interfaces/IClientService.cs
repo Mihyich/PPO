96d7b5b801419903c1898cc8406bf5d1ab1c4f14
GrizlyBear <michail.zevahin@gmail.com>
2025-03-23 16:24:12 +0300

    Объявлены базовые методы сервиса обработки клиентов. Введены предположительные коды ошибок

--- MetroGid/Services/Interfaces/IClientService.cs
+++ MetroGid/Services/Interfaces/IClientService.cs
@@ -1,7 +1,25 @@
 namespace MetroGid.Services.Interfaces
 {
+    public enum ClientServiceResult
+    {
+        SUCCESS = 0,
+        ILLEGAL_LOGIN, // не выполнены правила задания логина
+        ILLEGAL_PASSWORD, // не выполнены правила задания пароля
+        ILLEGAL_MAIL, // не выполнены правила задания почты
+        LOGIN_BUSY, // пользователь с таким логином уже существует
+        MAIL_BUSY, // пользователь с такой почтой уже существует
+        INVALID_SING_DATA // неверный логин и/или пароль
+    }
+    
     public interface IClientService
     {
-        
+        // Создание пользователя в БД
+        ClientServiceResult Auth(string login, string password, string mail);
+        // Вход
+        ClientServiceResult Sing_in(string login, string password);
+        // Выход
+        ClientServiceResult Sing_out(string login, string password);
+        // Удаление пользователя из БД
+        ClientServiceResult Log_out(string login, string password);
     }
 }
