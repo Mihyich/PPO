2b09931d820e0b5c497d819f68f0175468b8bfff
GrizlyBear <michail.zevahin@gmail.com>
2025-03-26 10:07:40 +0300

    Изменены параметры методов

--- MetroGid/Services/Interfaces/IControlService.cs
+++ MetroGid/Services/Interfaces/IControlService.cs
@@ -18,29 +18,29 @@ namespace MetroGid.Services.Interfaces
     public interface IControlService
     {
         // Изменение атрибутов таблицы Chart
-        ControlServiceResult UpdateChartTitle(ClientRole role, int id, string title);
-        ControlServiceResult UpdateChartCity(ClientRole role, int id, string city);
-        ControlServiceResult UpdateChartSvg_inst(ClientRole role, int id, string svg_inst);
+        ControlServiceResult UpdateChartTitle(string login, string password, int id, string title);
+        ControlServiceResult UpdateChartCity(string login, string password, int id, string city);
+        ControlServiceResult UpdateChartSvg_inst(string login, string password, int id, string svg_inst);
 
         // Изменение атрибутов таблицы Branch
-        ControlServiceResult UpdateBranchTitle(ClientRole role, int id, string title);
-        ControlServiceResult UpdateBranchColor(ClientRole role, int id, int color);
-        ControlServiceResult UpdateBranchAccessType(ClientRole role, int id, AccessType access_type);
+        ControlServiceResult UpdateBranchTitle(string login, string password, int id, string title);
+        ControlServiceResult UpdateBranchColor(string login, string password, int id, int color);
+        ControlServiceResult UpdateBranchAccessType(string login, string password, int id, AccessType access_type);
 
         // Изменение атрибутов таблицы Station
-        ControlServiceResult UpdateStationTitle(ClientRole role, int id, string title);
-        ControlServiceResult UpdateStationOccupancy(ClientRole role, int id, int occupancy);
-        ControlServiceResult UpdateStationAccessType(ClientRole role, int id, AccessType access_type);
-        ControlServiceResult UpdateStationOpenTime(ClientRole role, int id, string time); // Время ожидается конвертируемым в TimeOnly
-        ControlServiceResult UpdateStationCloseTime(ClientRole role, int id, string time); // Время ожидается конвертируемым в TimeOnly
+        ControlServiceResult UpdateStationTitle(string login, string password, int id, string title);
+        ControlServiceResult UpdateStationOccupancy(string login, string password, int id, int occupancy);
+        ControlServiceResult UpdateStationAccessType(string login, string password, int id, AccessType access_type);
+        ControlServiceResult UpdateStationOpenTime(string login, string password, int id, string time); // Время ожидается конвертируемым в TimeOnly
+        ControlServiceResult UpdateStationCloseTime(string login, string password, int id, string time); // Время ожидается конвертируемым в TimeOnly
 
         // Изменение атрибутов таблицы Transition
-        ControlServiceResult UpdateTransitionOccupancy(ClientRole role, int id, int occupancy);
-        ControlServiceResult UpdateTransitionAccessType(ClientRole role, int id, AccessType access_type);
-        ControlServiceResult UpdateTransitionOpenTime(ClientRole role, int id, string time); // Время ожидается конвертируемым в TimeOnly
-        ControlServiceResult UpdateTransitionCloseTime(ClientRole role, int id, string time); // Время ожидается конвертируемым в TimeOnly
+        ControlServiceResult UpdateTransitionOccupancy(string login, string password, int id, int occupancy);
+        ControlServiceResult UpdateTransitionAccessType(string login, string password, int id, AccessType access_type);
+        ControlServiceResult UpdateTransitionOpenTime(string login, string password, int id, string time); // Время ожидается конвертируемым в TimeOnly
+        ControlServiceResult UpdateTransitionCloseTime(string login, string password, int id, string time); // Время ожидается конвертируемым в TimeOnly
 
         // Изменение атрибутов таблицы StationStation
-        ControlServiceResult UpdateStationStationDuration(ClientRole role, int id, string duration); // Время ожидается конвертируемым в TimeOnly
+        ControlServiceResult UpdateStationStationDuration(string login, string password, int id, string duration); // Время ожидается конвертируемым в TimeOnly
     }
 }
