eb2b11b8416004ab6fd61414baa0d85377cd65ca
GrizlyBear <michail.zevahin@gmail.com>
2025-04-08 12:55:54 +0300

    Добавлен конструктор доменной сущности Chart

--- MetroGid/Services/Models/Chart.cs
+++ MetroGid/Services/Models/Chart.cs
@@ -1,10 +1,10 @@
 namespace MetroGid.Services.Models
 {
-    public class Chart
+    public class Chart(string title, string city, string svg_inst)
     {
-        public string Title {get; set;} = string.Empty;
-        public string City {get; set;} = string.Empty;
-        public string SvgInst {get; set;} = string.Empty;
+        public string Title { get; set; } = title;
+        public string City { get; set; } = city;
+        public string SvgInst { get; set; } = svg_inst;
         public List<Branch> Branches {get; set;} = [];
     }
 }
