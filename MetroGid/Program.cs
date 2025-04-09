a9caa46b92eae3ee88cda3c762364606c579f5cb
GrizlyBear <michail.zevahin@gmail.com>
2025-04-09 22:52:06 +0300

    Гойда

--- MetroGid/Program.cs
+++ MetroGid/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using MetroGid.Services.Models;
+using MetroGid.Services.Utilities;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        BuilderChart builder = new();
+        DirectorChartJson director = new(builder, "/home/mihail/Рабочий стол/BMSTU/PPO/temp_cities/Minsk/init.json");
+        Chart? chart = director.Construct();
+
+        if (chart != null)
+        {
+            chart.Searcher = new StrategySearchRouteBFS();
+            Station? stationA = chart.GetStation("Московская линия", "Московская");
+            Station? stationB = chart.GetStation("Зеленолужская линия", "Площадь Франтишка Богушевича");
+
+            if (stationA != null && stationB != null)
+            {
+                Route? route = chart.Search(stationA, stationB);
+                route?.Output();
+            }
+        }
     }
 }
