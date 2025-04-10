b4be5637945ddb40c638b19218866e7f98902725
GrizlyBear <michail.zevahin@gmail.com>
2025-04-10 23:28:10 +0300

    Созданы методы вычисления временных затрат

---
+++ MetroGid/Services/Utilities/TimeMeas.cs
@@ -0,0 +1,22 @@
+using MetroGid.Services.Models;
+
+namespace MetroGid.Services.Utilities
+{
+    public static class TimeMeas
+    {
+        public static TimeSpan Measure(Station s1, Railway r) =>
+            WaitOnStation(s1) + r.Duration.ToTimeSpan();
+
+        public static TimeSpan Measure(Station s1, Transition t, Station s2) =>
+            MoveOnStation(s1) + MoveOnTransition(t) + MoveOnStation(s2);
+
+        private static TimeSpan WaitOnStation(Station s) => 
+            TimeSpan.FromSeconds(6 * Math.Max(s.Occupancy, 1));
+
+        private static TimeSpan MoveOnStation(Station s) =>
+            TimeSpan.FromSeconds(10 * Math.Max(s.Occupancy, 1));
+
+        private static TimeSpan MoveOnTransition(Transition t) =>
+            TimeSpan.FromSeconds(t.Duration.ToTimeSpan().TotalSeconds * Math.Max(t.Occupancy / 2, 1));
+    }
+}
