b24002b91a853c43ee2c2843ddf3ee3af62c3b82
GrizlyBear <michail.zevahin@gmail.com>
2025-04-12 21:00:19 +0300

    Расширен тип chaintype

--- db/querries/create_types.sql
+++ db/querries/create_types.sql
@@ -18,4 +18,4 @@ $$ LANGUAGE plpgsql;
 
 SELECT recreate_enum_type('roletype', ARRAY['UNSIGNED', 'SIGNED', 'DUTY']);
 SELECT recreate_enum_type('accesstype', ARRAY['ACCESSIBLE', 'INACCESSIBLE']);
-SELECT recreate_enum_type('chaintype', ARRAY['Station', 'Transfer']);
+SELECT recreate_enum_type('chaintype', ARRAY['STATION', 'TRANSITION', 'RAILWAY']);
