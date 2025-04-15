4d470cc4064f8358808d175e11299124c5b2ddaf
GrizlyBear <michail.zevahin@gmail.com>
2025-04-15 18:36:25 +0300

    Заданы первичные методы интерфейса репозитория Route

---
+++ MetroGid/DBA/Interfaces/IRouteRepository.cs
@@ -0,0 +1,22 @@
+using MetroGid.Services.Models;
+
+namespace MetroGid.DBA.Interfaces
+{
+    public interface IRouteRepository
+    {
+        Task<int> AddAsync(Route route, int clientId, int chartId);
+
+        Task<int> GetIdAsync(string title, int clientId);
+        Task<Route?> GetByIdAsync(int id);
+        Task<List<string>?> GetAllTitlesForClientAsync(int clientId);
+        Task<List<string>?> GetAllTitlesForClientOfChartAsync(int clientId, int chartId);
+        Task<List<Route>?> GetAllForClientIdAsync(int clientId);
+        Task<List<Route>?> GetAllForClientOfChartIdAsync(int clientId, int chartId);
+
+        Task UpdateAsync(int id, Route route);
+
+        Task DeleteAsync(int id);
+
+        Task<bool> IsTitleUniqueAsync(string title, int clientId);
+    }
+}
