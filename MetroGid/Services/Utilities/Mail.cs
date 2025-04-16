83f2f90cba208abeabf9ddd5658663064479f9c2
GrizlyBear <michail.zevahin@gmail.com>
2025-04-16 09:21:02 +0300

    Статическая проверка mail

---
+++ MetroGid/Services/Utilities/Mail.cs
@@ -0,0 +1,14 @@
+using System.Text.RegularExpressions;
+
+namespace MetroGid.Services.Utilities
+{
+    public static class Mail
+    {
+        public static bool IsCorrect(string mail)
+        {
+            string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+            Match isMatch = Regex.Match(mail, pattern, RegexOptions.IgnoreCase);
+            return isMatch.Success;
+        }
+    }
+}
