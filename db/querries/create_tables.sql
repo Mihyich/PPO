341cb3fb8350e266eb877235bdbefbf3be099859
GrizlyBear <michail.zevahin@gmail.com>
2025-04-16 23:54:27 +0300

    Переход на примитивные типы данных, добавление ограничений

--- db/querries/create_tables.sql
+++ db/querries/create_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS Client (
 	client_login VARCHAR(255),
 	client_password VARCHAR(255),
 	mail VARCHAR(255),
-	role_type "roletype"
+	role_type VARCHAR(8) NOT NULL CHECK (role_type IN ('UNSIGNED', 'SIGNED', 'DUTY'))
 );
 
 CREATE TABLE IF NOT EXISTS Chart (
@@ -18,16 +18,16 @@ CREATE TABLE IF NOT EXISTS Chart (
 CREATE TABLE IF NOT EXISTS Branch (
 	ID SERIAL PRIMARY KEY,
 	title VARCHAR(255),
-	color INT, -- HEX число [0, 16777215]
-	access_type "accesstype"
+	color INT NOT NULL CHECK (color >= 0 AND color <= 16777215),
+	access_type VARCHAR(12) NOT NULL CHECK (access_type IN ('ACCESSIBLE', 'INACCESSIBLE'))
 );
 
 CREATE TABLE IF NOT EXISTS Station (
 	ID SERIAL PRIMARY KEY,
 	dutyID INT,
 	title VARCHAR(255),
-	occupancy INT, -- [0, 10]
-	access_type "accesstype",
+	occupancy INT NOT NULL CHECK (occupancy >= 0 AND occupancy <= 10),
+	access_type VARCHAR(12) NOT NULL CHECK (access_type IN ('ACCESSIBLE', 'INACCESSIBLE')),
 	open_time TIME,
 	close_time TIME,
 	FOREIGN KEY (dutyID) REFERENCES Client (ID)
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS Station (
 CREATE TABLE IF NOT EXISTS Transition (
 	ID SERIAL PRIMARY KEY,
 	dutyID INT,
-	occupancy INT, -- [0, 10]
-	access_type "accesstype",
+	occupancy INT NOT NULL CHECK (occupancy >= 0 AND occupancy <= 10),
+	access_type VARCHAR(12) NOT NULL CHECK (access_type IN ('ACCESSIBLE', 'INACCESSIBLE')),
 	duration TIME,
 	open_time TIME,
 	close_time TIME,
@@ -46,77 +46,89 @@ CREATE TABLE IF NOT EXISTS Transition (
 
 CREATE TABLE IF NOT EXISTS Way (
 	ID SERIAL PRIMARY KEY,
-	clientID INT,
-	chartID INT,
+	clientID INT NOT NULL,
+	chartID INT NOT NULL,
 	title VARCHAR(255),
 	init_date TIMESTAMP,
 	FOREIGN KEY (clientID) REFERENCES Client (ID),
-	FOREIGN KEY (chartID) REFERENCES Chart (ID)
+	FOREIGN KEY (chartID) REFERENCES Chart (ID),
+	UNIQUE (clientID, title)
 );
 
 CREATE TABLE IF NOT EXISTS WayItem (
 	ID SERIAL PRIMARY KEY,
-	wayID INT,
-	chain_type "wayitemtype",
+	wayID INT NOT NULL,
+	way_type VARCHAR(10) NOT NULL CHECK (way_type IN ('STATION', 'TRANSITION', 'RAILWAY')),
 	step_nomer INT,
-	FOREIGN KEY (wayID) REFERENCES Way (ID)
+	FOREIGN KEY (wayID) REFERENCES Way (ID),
+	UNIQUE (wayID, step_nomer)
 );
 
 -- Связующие таблицы
 
 CREATE TABLE IF NOT EXISTS ChartBranch (
 	ID SERIAL PRIMARY KEY,
-	chartID INT,
-	branchID INT,
+	chartID INT NOT NULL,
+	branchID INT NOT NULL,
 	FOREIGN KEY (chartID) REFERENCES Chart (ID),
-	FOREIGN KEY (branchID) REFERENCES Branch (ID)
+	FOREIGN KEY (branchID) REFERENCES Branch (ID),
+	UNIQUE (chartID, branchID)
 );
 
 CREATE TABLE IF NOT EXISTS BranchStation (
 	ID SERIAL PRIMARY KEY,
-	branchID INT,
-	stationID INT,
+	branchID INT NOT NULL,
+	stationID INT NOT NULL,
 	FOREIGN KEY (branchID) REFERENCES Branch (ID),
-	FOREIGN KEY (stationID) REFERENCES Station (ID)
+	FOREIGN KEY (stationID) REFERENCES Station (ID),
+	UNIQUE (branchID, stationID)
 );
 
 CREATE TABLE IF NOT EXISTS Railway (
 	ID SERIAL PRIMARY KEY,
-	fromID INT,
-	toID INT,
+	fromID INT NOT NULL,
+	toID INT NOT NULL,
 	duration TIME,
 	FOREIGN KEY (fromID) REFERENCES Station (ID),
-	FOREIGN KEY (toID) REFERENCES Station (ID)
+	FOREIGN KEY (toID) REFERENCES Station (ID),
+	UNIQUE (fromID, toID),
+	CHECK (fromID <> toID)
 );
 
 CREATE TABLE IF NOT EXISTS StationTransition (
 	ID SERIAL PRIMARY KEY,
-	stationID INT,
-	transitionID INT,
+	stationID INT NOT NULL,
+	transitionID INT NOT NULL,
 	FOREIGN KEY (stationID) REFERENCES Station (ID),
 	FOREIGN KEY (transitionID) REFERENCES Transition (ID)
 );
 
 CREATE TABLE IF NOT EXISTS WayItemStation (
 	ID SERIAL PRIMARY KEY,
-	stationID INT,
-	wayitemID INT,
+	stationID INT NOT NULL,
+	wayitemID INT NOT NULL,
 	FOREIGN KEY (stationID) REFERENCES Station (ID),
-	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID)
+	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID),
+	UNIQUE (stationID, wayitemID),
+	UNIQUE (wayitemID)
 );
 
 CREATE TABLE IF NOT EXISTS WayItemRailway (
 	ID SERIAL PRIMARY KEY,
-	railwayID INT,
-	wayitemID INT,
+	railwayID INT NOT NULL,
+	wayitemID INT NOT NULL,
 	FOREIGN KEY (railwayID) REFERENCES Railway (ID),
-	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID)
+	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID),
+	UNIQUE (railwayID, wayitemID),
+    UNIQUE (wayitemID)
 );
 
 CREATE TABLE IF NOT EXISTS WayItemTransition (
 	ID SERIAL PRIMARY KEY,
-	transitionID INT,
-	wayitemID INT,
+	transitionID INT NOT NULL,
+	wayitemID INT NOT NULL,
 	FOREIGN KEY (transitionID) REFERENCES Transition (ID),
-	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID)
+	FOREIGN KEY (wayitemID) REFERENCES WayItem (ID),
+	UNIQUE (transitionID, wayitemID),
+    UNIQUE (wayitemID)
 );
