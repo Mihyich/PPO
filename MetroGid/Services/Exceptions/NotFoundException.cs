8c335470080037106fbd44480868ead9fac1d028
GrizlyBear <michail.zevahin@gmail.com>
2025-04-16 09:26:37 +0300

    Начата миграция кодов ошибок для NotFoundException

--- MetroGid/Services/Exceptions/NotFoundException.cs
+++ MetroGid/Services/Exceptions/NotFoundException.cs
@@ -2,5 +2,13 @@ using System.Net;
 
 namespace MetroGid.Services.Exceptions
 {
-    public class NotFoundException(string entity) : DomainException($"{entity} not found", HttpStatusCode.NotFound) {}
+    public class NotFoundException(NotFoundException.ErrorType reason, string message) : DomainException(message, HttpStatusCode.NotFound)
+    {
+        public enum ErrorType
+        {
+            ClientLost // Пользователь не найден
+        }
+
+        public ErrorType Reason { get; } = reason;
+    }
 }
