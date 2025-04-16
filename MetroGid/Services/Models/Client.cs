09a34f9521de8745228e4794684e6c5b761cc3ac
GrizlyBear <michail.zevahin@gmail.com>
2025-04-16 09:21:30 +0300

    Добавлен конструктор в доменную сущность Client

--- MetroGid/Services/Models/Client.cs
+++ MetroGid/Services/Models/Client.cs
@@ -1,10 +1,10 @@
 namespace MetroGid.Services.Models
 {
-    public class Client
+    public class Client(string login, string password, string mail)
     {
-        public string Login {get; set;} = string.Empty;
-        public string Password {get; set;} = string.Empty;
-        public string Mail {get; set;} = string.Empty;
+        public string Login {get; set;} = login;
+        public string Password {get; set;} = password;
+        public string Mail {get; set;} = mail;
         public RoleType Role {get; set;} = RoleType.UNSIGNED;
     }
 }
