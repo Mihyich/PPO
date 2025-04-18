d0124e7c2e40d516367c8d78e87134c4958c2988
GrizlyBear <michail.zevahin@gmail.com>
2025-04-18 15:46:23 +0300

    Созданы дополнительные DTO сущности

--- MetroGid/Controllers/DTO/RailwayDTO.cs
+++ MetroGid/Controllers/DTO/RailwayDTO.cs
@@ -1,7 +1,12 @@
 namespace MetroGid.Controllers.DTO
 {
-    public class RailwayDTO
+    public class RailwayDTO(
+        string prev_station_title, string next_station_title,
+        TimeOnly duration
+    )
     {
-        
+        public string PrevStationTitle { get; set; } = prev_station_title;
+        public string NextStationTile { get; set; } = next_station_title;
+        public TimeOnly Duration = duration;
     }
 }
--- MetroGid/Controllers/DTO/TransitionDTO.cs
+++ MetroGid/Controllers/DTO/TransitionDTO.cs
@@ -1,7 +1,22 @@
 namespace MetroGid.Controllers.DTO
 {
-    public class TransitionDTO
+    public class TransitionDTO(
+        int occupancy, AccessTypeDTO type, TimeOnly duration,
+        TimeOnly open_time, TimeOnly close_time,
+        string from_station_title, string from_branch_title,
+        string to_station_title, string to_branch_title
+    )
     {
-        
+        public int Occupancy { get; set; } = occupancy;
+        public AccessTypeDTO Type { get; set; } = type;
+        public TimeOnly Duration { get; set; } = duration;
+        public TimeOnly OpenTime { get; set; } = open_time;
+        public TimeOnly CloseTime { get; set; } = close_time;
+
+        public string FromStationTitle { get; set; } = from_station_title;
+        public string FromBranchTitle { get; set; } = from_branch_title;
+
+        public string ToStationTitle { get; set; } = to_station_title;
+        public string ToBranchTitle { get; set; } = to_branch_title;
     }
 }
