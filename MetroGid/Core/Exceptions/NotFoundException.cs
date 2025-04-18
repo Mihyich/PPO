031b201ac3808b499783b204462af8e3803f8594
GrizlyBear <michail.zevahin@gmail.com>
2025-04-18 13:26:13 +0300

    Перестановка перечислений, пополнение причин исключений

--- MetroGid/Core/Exceptions/NotFoundException.cs
+++ MetroGid/Core/Exceptions/NotFoundException.cs
@@ -8,11 +8,12 @@ namespace MetroGid.Core.Exceptions
         {
             ClientLost, // Пользователь не найден
             ChartIdMisMatch, // Неверный айди Chart
-            ChartCityTitleMisMatch, // Не существует схемы в таком городе с таким названием
             BranchIdMisMatch, // Неверный айди Branch
-            BranchTitleChartIdMisMatch, // Не найдена ветка с таким названием в схеме с таким айди
             StationIdMisMatch, // Неверный айди Station
-            StationTitleBranchIdMisMatch, // Не найдена станция с таким названием на ветка с таким айди
+            TransitionIdMisMatch, // Неверный айди Transition
+            ChartCityTitleMisMatch, // Не существует схемы в таком городе с таким названием
+            BranchTitleChartIdMisMatch, // Не найдена ветка с таким названием в схеме с таким айди
+            StationTitleBranchIdMisMatch // Не найдена станция с таким названием на ветка с таким айди
         }
 
         public ErrorType Reason { get; } = reason;
