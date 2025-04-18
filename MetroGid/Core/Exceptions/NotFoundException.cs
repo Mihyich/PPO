f7c077409d718329418713e7429d580e45806f50
GrizlyBear <michail.zevahin@gmail.com>
2025-04-18 12:37:37 +0300

    Обновление списка возможных причин возникновения исключения NotFound

--- MetroGid/Core/Exceptions/NotFoundException.cs
+++ MetroGid/Core/Exceptions/NotFoundException.cs
@@ -6,7 +6,13 @@ namespace MetroGid.Core.Exceptions
     {
         public enum ErrorType
         {
-            ClientLost // Пользователь не найден
+            ClientLost, // Пользователь не найден
+            ChartIdMisMatch, // Неверный айди Chart
+            ChartCityTitleMisMatch, // Не существует схемы в таком городе с таким названием
+            BranchIdMisMatch, // Неверный айди Branch
+            BranchTitleChartIdMisMatch, // Не найдена ветка с таким названием в схеме с таким айди
+            StationIdMisMatch, // Неверный айди Station
+            StationTitleBranchIdMisMatch, // Не найдена станция с таким названием на ветка с таким айди
         }
 
         public ErrorType Reason { get; } = reason;
