26f091420e0e374109f6c5a0669d981442e4d4d6
GrizlyBear <michail.zevahin@gmail.com>
2025-04-19 22:54:28 +0300

    Модифицирован сервис клиента с учетом Vistor'а исключений и будущей исключение-бросательной способностью репозиториев

--- MetroGid/Core/Services/ClientService.cs
+++ MetroGid/Core/Services/ClientService.cs
@@ -1,9 +1,10 @@
 using MetroGid.Controllers.DTO;
 using MetroGid.Controllers.Interfaces;
-using MetroGid.Core.Exceptions;
+using MetroGid.Core.Exceptions.Classification;
+using MetroGid.Core.Exceptions.Concrete;
 using MetroGid.Core.Models;
 using MetroGid.Core.Utilities;
-using MetroGid.DBA.Interfaces; 
+using MetroGid.DBA.Interfaces;
 
 namespace MetroGid.Core.Services
 {
@@ -14,77 +15,57 @@ namespace MetroGid.Core.Services
         public async Task<int> Reg(string login, string password, string mail)
         {
             if (!Login.IsCorrect(login))
-                throw new RegistrationException(
-                    RegistrationException.ErrorType.ILLEGAL_LOGIN,
-                    "Логин должен содержать 4 - 255 символов"
+                throw new LoginValidationException(
+                    "Логин должен содержать 4 - 255 символов",
+                    ExceptionType.Quiet,
+                    ExceptionReason.ValidationFailed
                 );
 
             if (!Password.IsCorrect(password))
-                throw new RegistrationException(
-                    RegistrationException.ErrorType.ILLEGAL_PASSWORD,
-                    "Пароль должен содержать 4 - 255 символов"
+                throw new PasswordValidationException(
+                    "Пароль должен содержать 4 - 255 символов",
+                    ExceptionType.Quiet,
+                    ExceptionReason.ValidationFailed
                 );
 
             if (!Mail.IsCorrect(mail))
-                throw new RegistrationException(
-                    RegistrationException.ErrorType.ILLEGAL_MAIL,
-                    "Не удалось успешно валидировать почту"
+                throw new MailValidationException(
+                    "Не удалось успешно валидировать почту",
+                    ExceptionType.Quiet,
+                    ExceptionReason.ValidationFailed
                 );
 
             if (await ClientRepo.IsLoginExistsAsync(login))
-                throw new RegistrationException(
-                    RegistrationException.ErrorType.LOGIN_BUSY,
-                    $"Логин '{login}' уже занят"
+                throw new ItemAlreadyInUseException(
+                    $"Логин '{login}' уже занят",
+                    ExceptionType.Quiet,
+                    ExceptionReason.ItemAlreadyInUse
                 );
 
             if (await ClientRepo.IsMailExistsAsync(mail))
-                throw new RegistrationException(
-                    RegistrationException.ErrorType.MAIL_BUSY,
-                    $"Почта '{mail}' уже занята"
+                throw new ItemAlreadyInUseException(
+                    $"Почта '{mail}' уже занята",
+                    ExceptionType.Quiet,
+                    ExceptionReason.ItemAlreadyInUse
                 );
 
             Client client = new(login, password, mail);
             int id = await ClientRepo.AddAsync(client);
-
             return id;
         }
 
-        public async Task UnReg(string login, string password, string mail)
-        {
-            Client? client = await ClientRepo.GetByCredentialsAsync(login, password, mail) ??
-                throw new NotFoundException(
-                    NotFoundException.ErrorType.ClientLost,
-                    $"Пользователь не найден:\n  Логин: '{login}'\n  Пароль: '{password}'\n  Почта: '{mail}'"
-                );
+        public async Task UnReg(string login, string password, string mail) =>
+            await ClientRepo.DeleteAsync(
+                await ClientRepo.GetIdByCredentialsAsync(login, password, mail));
 
-            int id = await ClientRepo.GetIdAsync(client);
-            await ClientRepo.DeleteAsync(id);
-        }
+        public async Task<int> SingIn(string login, string password, string mail) =>
+            await ClientRepo.GetIdAsync(
+                await ClientRepo.GetByCredentialsAsync(login, password, mail));
 
-        public async Task<int> SingIn(string login, string password, string mail)
-        {
-            Client? client = await ClientRepo.GetByCredentialsAsync(login, password, mail) ??
-                throw new NotFoundException(
-                    NotFoundException.ErrorType.ClientLost,
-                    $"Пользователь не найден:\n  Логин: '{login}'\n  Пароль: '{password}'\n  Почта: '{mail}'"
-                );
+        public async Task SingOut(string login, string password, string mail) =>
+            await ClientRepo.GetByCredentialsAsync(login, password, mail);
 
-            return await ClientRepo.GetIdAsync(client);
-        }
-
-        public async Task SingOut(string login, string password, string mail)
-        {
-            _ = await ClientRepo.GetByCredentialsAsync(login, password, mail) ??
-                throw new NotFoundException(
-                    NotFoundException.ErrorType.ClientLost,
-                    $"Пользователь не найден:\n  Логин: '{login}'\n  Пароль: '{password}'\n  Почта: '{mail}'"
-                );
-        }
-
-        public async Task<RoleTypeDTO> GetRole(string login, string password, string mail)
-        {
-            Client? client = await ClientRepo.GetByCredentialsAsync(login, password, mail);
-            return client != null ? CntRoleType.Convert(client.Role) : RoleTypeDTO.UNSIGNED;
-        }
+        public async Task<RoleTypeDTO> GetRole(string login, string password, string mail) =>
+            CntRoleType.Convert((await ClientRepo.GetByCredentialsAsync(login, password, mail)).Role);
     }
 }
