e43d382e6d8982ddf5b3421b843e6ba7031cb97c
GrizlyBear <michail.zevahin@gmail.com>
2025-04-19 21:40:01 +0300

    Пересмотрена технология исключений; Переход на Visitor

--- MetroGid/Core/Exceptions/ConflictException.cs
+++
@@ -1,6 +0,0 @@
-using System.Net;
-
-namespace MetroGid.Core.Exceptions
-{
-    public class ConflictException(string message) : DomainException(message, HttpStatusCode.Conflict) { }
-}
--- MetroGid/Core/Exceptions/DomainException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace MetroGid.Core.Exceptions
-{
-    public abstract class DomainException(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest) : Exception(message)
-    {
-        public HttpStatusCode StatusCode { get; } = statusCode;
-    }
-}
