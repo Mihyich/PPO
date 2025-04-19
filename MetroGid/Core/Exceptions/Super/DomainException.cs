307c69a47e05ec14678805bf8c300e55bd082f10
GrizlyBear <michail.zevahin@gmail.com>
2025-04-19 21:45:33 +0300

    Внедрение Visitor'а в абстрактный пользовательский класс исключений

---
+++ MetroGid/Core/Exceptions/Super/DomainException.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+using MetroGid.Core.Exceptions.Classification;
+using MetroGid.Core.Exceptions.Interfaces;
+
+namespace MetroGid.Core.Exceptions.Super
+{
+    public abstract class SuperException(
+        string message,
+        ExceptionType excType,
+        ExceptionReason excReason,
+        [CallerFilePath] string filePath = "",
+        [CallerLineNumber] int lineNumber = 0,
+        Exception? innerException = null) : Exception(message, innerException), IExceptionAccepter
+    {
+        public DateTime OccurAt { get; } = DateTime.UtcNow;
+        public string FilePath { get; } = filePath;
+        public int LineNumber { get; } = lineNumber;
+        public readonly ExceptionType ExcType = excType;
+        public readonly ExceptionReason ExcReason = excReason;
+
+        public string GetInfo() =>
+            $"[{ExcType}]:[{OccurAt}]:({Path.GetFileName(FilePath)}):[{LineNumber}]:{base.Message}";
+
+        public abstract void Accept(IExceptionVisitor visitor);
+    }
+}
