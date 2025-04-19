3094319c77f4c01bb4b36b58cb169242bc368fd4
GrizlyBear <michail.zevahin@gmail.com>
2025-04-20 02:30:13 +0300

    Первичная реализация сервиса Route

---
+++ MetroGid/Core/Services/RouteService.cs
@@ -0,0 +1,42 @@
+using MetroGid.Controllers.DTO;
+using MetroGid.Controllers.Interfaces;
+using MetroGid.Core.Models;
+using MetroGid.Core.Utilities;
+using MetroGid.DBA.Interfaces;
+
+namespace MetroGid.Core.Services
+{
+    public class RouteService(IChartRepository chartRepo, IRouteRepository routeRepo) : IRouteService
+    {
+        private readonly IChartRepository ChartRepo = chartRepo;
+        private readonly IRouteRepository RouteRepo = routeRepo;
+
+        public async Task<RouteDTO> SearchRoute(
+            string city, string chartTitle,
+            string branchSrcTitle, string stationSrcTitle,
+            string branchDstTitle, string stationDstTitle)
+        {
+            BuilderChart builder = new();
+            DirectorChartJson director = new(
+                builder,
+                await ChartRepo.GetChartJsonByIdAsync(
+                    await ChartRepo.GetChartIdAsync(city, chartTitle))
+            );
+
+            Chart? chart = director.Construct();
+            chart.Searcher = new StrategySearchRouteDijkstra();
+            Station? src = chart?.GetStation(branchSrcTitle, stationSrcTitle);
+            Station? dst = chart?.GetStation(branchDstTitle, stationDstTitle);
+
+            return CntRoute.Convert(chart?.Search(src, dst));
+        }
+
+        public Task SaveRoute(int clientId, RouteDTO route, int chartId) =>
+            RouteRepo.AddAsync(CntRoute.Convert(route), clientId, chartId);
+
+        public Task<List<RouteDTO>> LookForSavedRoutesInChart(int clientId, int chartId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
