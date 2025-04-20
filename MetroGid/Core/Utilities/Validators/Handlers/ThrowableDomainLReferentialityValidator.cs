679936a1915f2aa585c5578923a57cebbdd8824d
GrizlyBear <michail.zevahin@gmail.com>
2025-04-20 13:52:04 +0300

    Объявлен валидатор ссылочной связности доменных сущностей

---
+++ MetroGid/Core/Utilities/Validators/Handlers/ThrowableDomainLReferentialityValidator.cs
@@ -0,0 +1,33 @@
+using MetroGid.Core.Models;
+using MetroGid.Core.Utilities.Validators.Interfaces;
+
+namespace MetroGid.Core.Utilities.Validators.Handlers
+{
+    public class ThrowableDomainReferentialityValidator : IDomainValidatorVisitor
+    {
+        public void Visit(Chart chart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Branch branch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Station station)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Railway railway)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Transition transition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
