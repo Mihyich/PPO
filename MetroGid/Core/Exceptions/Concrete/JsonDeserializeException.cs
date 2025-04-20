586a1d3cb0e0f1018e6acd94a3719335388ab9f2
GrizlyBear <michail.zevahin@gmail.com>
2025-04-20 10:00:00 +0300

    Создано исключение валидации десериализации Json файла

---
+++ MetroGid/Core/Exceptions/Concrete/JsonDeserializeException.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+using MetroGid.Core.Exceptions.Classification;
+using MetroGid.Core.Exceptions.Interfaces;
+using MetroGid.Core.Exceptions.Super;
+
+namespace MetroGid.Core.Exceptions.Concrete
+{
+    public class JsonDeserializeException(
+        string message,
+        ExceptionType extType,
+        ExceptionReason excReason,
+        [CallerFilePath] string filePath = "",
+        [CallerLineNumber] int lineNumber = 0,
+        Exception? innerException = null
+    ) : SuperException(
+        message, extType, excReason,
+        filePath, lineNumber, innerException)
+    {
+        public override void Accept(IExceptionVisitor visitor) => visitor.Visit(this);
+    }
+}
