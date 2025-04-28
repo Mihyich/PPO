31ae9ae0d8d2ab427d7968a9f31496792557dc7d
GrizlyBear <michail.zevahin@gmail.com>
2025-04-28 16:10:20 +0300

    Обновлен конструктор доменной сущности Client

--- MetroGid/Core/Models/Concrete/Client.cs
+++ MetroGid/Core/Models/Concrete/Client.cs
@@ -2,11 +2,11 @@ using MetroGid.Core.Models.Types;
 
 namespace MetroGid.Core.Models.Concrete
 {
-    public class Client(string login, string password, string mail)
+    public class Client(string login, string password, string mail, RoleType role = RoleType.UNSIGNED)
     {
         public string Login { get; set; } = login;
         public string Password { get; set; } = password;
         public string Mail { get; set; } = mail;
-        public RoleType Role { get; set; } = RoleType.UNSIGNED;
+        public RoleType Role { get; set; } = role;
     }
 }
