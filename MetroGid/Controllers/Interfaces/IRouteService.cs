00fca93846e1e4077d275eba346c74621da7cca6
GrizlyBear <michail.zevahin@gmail.com>
2025-05-01 12:38:59 +0300

    Обновлены необходимые параметры методов в сервисе маршрутов

--- MetroGid/Controllers/Interfaces/IRouteService.cs
+++ MetroGid/Controllers/Interfaces/IRouteService.cs
@@ -7,7 +7,8 @@ namespace MetroGid.Controllers.Interfaces
         Task<RouteDTO> SearchRoute(
             string city, string chartTitle,
             string branchSrcTitle, string stationSrcTitle,
-            string branchDstTitle, string stationDstTitle);
+            string branchDstTitle, string stationDstTitle,
+            TimeOnly startTime);
 
         Task SaveRoute(int clientId, RouteDTO route, int chartId);
         Task<List<RouteDTO>> LookForSavedRoutesInChart(int clientId, int chartId); // просмотр сохраненных маршрутов в конкретной схеме метро, у конктретного пользователя
