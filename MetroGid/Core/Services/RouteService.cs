8498ae1cf33019dcbe1f32ac60085e7b689bb9d5
GrizlyBear <michail.zevahin@gmail.com>
2025-05-01 12:40:51 +0300

    Пересмотрена логика методов в сервисах

--- MetroGid/Core/Services/RouteService.cs
+++ MetroGid/Core/Services/RouteService.cs
@@ -1,12 +1,22 @@
 using MetroGid.Controllers.DTO;
 using MetroGid.Controllers.Interfaces;
-using MetroGid.Core.Models;
+using MetroGid.Core.Exceptions.Classification;
+using MetroGid.Core.Exceptions.Concrete;
+using MetroGid.Core.Exceptions.Handlers;
+using MetroGid.Core.Exceptions.Loggers;
+using MetroGid.Core.Models.Concrete;
 using MetroGid.Core.Utilities;
+using MetroGid.Core.Utilities.Builders;
+using MetroGid.Core.Utilities.Directors;
+using MetroGid.Core.Utilities.Strategies;
+using MetroGid.Core.Utilities.Validators.Handlers;
 using MetroGid.DBA.Interfaces;
 
 namespace MetroGid.Core.Services
 {
-    public class RouteService(IChartRepository chartRepo, IRouteRepository routeRepo) : IRouteService
+    public class RouteService(
+        IChartRepository chartRepo,
+        IRouteRepository routeRepo) : IRouteService
     {
         private readonly IChartRepository ChartRepo = chartRepo;
         private readonly IRouteRepository RouteRepo = routeRepo;
@@ -14,24 +24,41 @@ namespace MetroGid.Core.Services
         public async Task<RouteDTO> SearchRoute(
             string city, string chartTitle,
             string branchSrcTitle, string stationSrcTitle,
-            string branchDstTitle, string stationDstTitle)
+            string branchDstTitle, string stationDstTitle,
+            TimeOnly startTime)
         {
-            // BuilderChart builder = new();
+            WarningHandlerException handler = new();
+            ExceptionMessenger logger = new();
 
-            // DirectorChartJson director = new(
-            //     builder,
-            //     await ChartRepo.GetChartJsonByIdAsync(
-            //         await ChartRepo.GetChartIdAsync(city, chartTitle))
-            // );
+            ThrowableDomainAttribsValidator DomainAttribsValidator = new(handler, logger);
+            ThrowableDomainReferentialityValidator DomainReferentialityValidator = new(handler, logger);
 
-            // Chart chart = director.Construct();
-            // chart.Searcher = new StrategySearchRouteDijkstra();
-            // Station? src = chart?.GetStation(branchSrcTitle, stationSrcTitle);
-            // Station? dst = chart?.GetStation(branchDstTitle, stationDstTitle);
+            BuilderChart builder = new(DomainAttribsValidator, DomainReferentialityValidator);
+            DirectorChartJson director = new(
+                builder,
+                await ChartRepo.GetChartJsonByIdAsync(await ChartRepo.GetChartIdAsync(city, chartTitle))
+            );
 
-            // return CntRoute.Convert(chart?.Search(src, dst));
+            StrategySearchRouteBase searcher = new StrategySearchRouteBFS();
+            Chart chart = director.Construct();
 
-            return default;
+            chart.Searcher = searcher;
+
+            Station src = chart.GetStation(branchSrcTitle, stationSrcTitle) ??
+                throw new ServiceRouteException(
+                    $"В схеме '{chart.Title}' в городе '{chart.City}' не найдена станция '{stationSrcTitle}' ветки '{branchSrcTitle}'",
+                    ExceptionType.Error,
+                    ExceptionReason.NotFound
+                );
+
+            Station dst = chart.GetStation(branchDstTitle, stationDstTitle) ??
+                throw new ServiceRouteException(
+                    $"В схеме '{chart.Title}' в городе '{chart.City}' не найдена станция '{stationDstTitle}' ветки '{branchDstTitle}'",
+                    ExceptionType.Error,
+                    ExceptionReason.NotFound
+                );
+
+            return CntRoute.Convert(chart.Search(src, dst, startTime));
         }
 
         public Task SaveRoute(int clientId, RouteDTO route, int chartId) =>
