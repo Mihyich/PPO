a8f72d5b08076df5cb86b5db05cfdf168fcb9418
GrizlyBear <michail.zevahin@gmail.com>
2025-05-02 15:42:39 +0300

    Добавлен обыный тест поиска маршрута по Москве

--- MetroGidTests/ServicesTests/RouteServiceTests.cs
+++ MetroGidTests/ServicesTests/RouteServiceTests.cs
@@ -13,7 +13,7 @@ namespace MetroGidTests.ServicesTests;
 public class RouteServiceTests
 {
     [Fact]
-    public async void UsualSearchingTest()
+    public async void UsualSearchingAdanaTest()
     {
         SuperHandlerException handler = new PassThroughHandlerException();
 
@@ -70,6 +70,108 @@ public class RouteServiceTests
                 new RouteStationItemDTO(new StationDTO("Акынджилар", "Линия 1", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30)))
             ],
             Duration = TimeSpan.FromMinutes(2 * 60 + 6)
+        };
+
+        mockChartRepo.Verify(x => x.GetChartJsonAsync(city, chartTitle), Times.Once);
+        Assert.True(route.Equals(expectedRoute));
+    }
+
+    [Fact]
+    public async void UsualSearchingMoscowTest()
+    {
+        SuperHandlerException handler = new PassThroughHandlerException();
+
+        IDomainValidatorVisitor domainAttribsValidator = new ThrowableDomainAttribsValidator(handler);
+        IDomainValidatorVisitor domainReferentialityValidator = new ThrowableDomainReferentialityValidator(handler);
+        
+        Mock<IChartRepository> mockChartRepo = new();
+        Mock<IRouteRepository> mockRouteRepo = new();
+
+        string city = "Москва";
+        string chartTitle = "Московский метрополитен";
+        string branchSrcTitle = "МЦД-2";
+        string stationSrcTitle = "Нахабино";
+        string branchDstTitle = "Замоскворецкая линия";
+        string stationDstTitle = "Алма-Атинская";
+        TimeOnly startTime = new(18, 0);
+
+        string currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName ?? string.Empty;
+        string filePath = Path.Combine(currentDirectory, "Cities", "Moscow", "chart.json");
+
+        mockChartRepo.Setup(x => x.GetChartJsonAsync(city, chartTitle)).ReturnsAsync(FileReader.ReadAll(filePath));
+        RouteService routeService = new(mockChartRepo.Object, mockRouteRepo.Object, handler);
+
+        RouteDTO route = await routeService.SearchRoute(city, chartTitle, branchSrcTitle, stationSrcTitle, branchDstTitle, stationDstTitle, startTime);
+
+        RouteDTO expectedRoute = new("Новый маршрут", city, chartTitle)
+        {
+            Path =
+            [
+                new RouteStationItemDTO(new StationDTO("Нахабино", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Нахабино", "Аникеевка", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Аникеевка", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Аникеевка", "Опалиха", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Опалиха", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Опалиха", "Красногорская", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Красногорская", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Красногорская", "Павшино", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Павшино", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Павшино", "Пенягино", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Пенягино", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Пенягино", "Волоколамская", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Волоколамская", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Волоколамская", "Трикотажная", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Трикотажная", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Трикотажная", "Тушинская", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Тушинская", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Тушинская", "Щукинская", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Щукинская", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("МЦД-2", "Щукинская", "Стрешнево", new TimeOnly(0, 9)))),
+                new RouteStationItemDTO(new StationDTO("Стрешнево", "МЦД-2", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                // пересадка =======================================================================================================================================
+                new RouteConnectionItemDTO(new TransitionConnectionDTO(new TransitionDTO(5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(0, 14), new TimeOnly(5, 30), new TimeOnly(1, 30), "Войковская", "Замоскворецкая линия", "Стрешнево", "МЦД-2"))),
+                // пересадка =======================================================================================================================================
+                new RouteStationItemDTO(new StationDTO("Войковская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Войковская", "Сокол", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Сокол", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Сокол", "Аэропорт", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Аэропорт", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Аэропорт", "Динамо", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Динамо", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Динамо", "Белорусская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Белорусская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Белорусская", "Маяковская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Маяковская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Маяковская", "Тверская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Тверская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Тверская", "Театральная", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Театральная", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Театральная", "Новокузнецкая", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Новокузнецкая", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Новокузнецкая", "Павелецкая", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Павелецкая", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Павелецкая", "Автозаводская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Автозаводская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Автозаводская", "Технопарк", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Технопарк", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Технопарк", "Коломенская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Коломенская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Коломенская", "Каширская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Каширская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Каширская", "Кантемировская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Кантемировская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Кантемировская", "Царицыно", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Царицыно", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Царицыно", "Орехово", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Орехово", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Орехово", "Домодедовская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Домодедовская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Домодедовская", "Красногвардейская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Красногвардейская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+                new RouteConnectionItemDTO(new RailwayConnectionDTO(new RailwayDTO("Замоскворецкая линия", "Красногвардейская", "Алма-Атинская", new TimeOnly(0, 5)))),
+                new RouteStationItemDTO(new StationDTO("Алма-Атинская", "Замоскворецкая линия", 5, AccessTypeDTO.ACCESSIBLE, new TimeOnly(5, 30), new TimeOnly(1, 30))),
+            ],
+            Duration = TimeSpan.FromHours(3) + TimeSpan.FromMinutes(49) + TimeSpan.FromSeconds(10)
         };
 
         mockChartRepo.Verify(x => x.GetChartJsonAsync(city, chartTitle), Times.Once);
