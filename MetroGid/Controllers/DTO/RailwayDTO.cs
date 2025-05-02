f960fb597868f370cd2dffaf5971d7094a678929
GrizlyBear <michail.zevahin@gmail.com>
2025-05-02 13:14:24 +0300

    Увеличено количество переменный для RailwayDTO

--- MetroGid/Controllers/DTO/RailwayDTO.cs
+++ MetroGid/Controllers/DTO/RailwayDTO.cs
@@ -1,10 +1,13 @@
 namespace MetroGid.Controllers.DTO;
 
 public class RailwayDTO(
-    string prev_station_title, string next_station_title,
+    string branchTitle,
+    string prev_station_title,
+    string next_station_title,
     TimeOnly duration
 )
 {
+    public string BranchTitle { get; set; } = branchTitle;
     public string PrevStationTitle { get; set; } = prev_station_title;
     public string NextStationTile { get; set; } = next_station_title;
     public TimeOnly Duration = duration;
