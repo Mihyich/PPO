600f4438c08a3286c555ac0e694536028c03d1dc
GrizlyBear <michail.zevahin@gmail.com>
2025-05-02 13:21:16 +0300

    Расширен набор предикатов RailwayDTO

--- MetroGid/Controllers/DTO/RailwayDTO.cs
+++ MetroGid/Controllers/DTO/RailwayDTO.cs
@@ -11,4 +11,16 @@ public class RailwayDTO(
     public string PrevStationTitle { get; set; } = prev_station_title;
     public string NextStationTile { get; set; } = next_station_title;
     public TimeOnly Duration = duration;
+
+
+    public override bool Equals(object? obj) =>
+        obj is RailwayDTO other &&
+        BranchTitle == other.BranchTitle &&
+        PrevStationTitle == other.PrevStationTitle &&
+        NextStationTile == other.NextStationTile &&
+        Duration == other.Duration;
+
+
+    public override int GetHashCode() =>
+        HashCode.Combine(BranchTitle, PrevStationTitle, NextStationTile, Duration.Ticks.ToString());
 }
