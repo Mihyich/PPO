9beed56b4a90b7dc566b290685d327b4bf07e8ce
GrizlyBear <michail.zevahin@gmail.com>
2025-05-02 13:25:19 +0300

    Расширен набор предикатов StationDTO

--- MetroGid/Controllers/DTO/StationDTO.cs
+++ MetroGid/Controllers/DTO/StationDTO.cs
@@ -1,10 +1,26 @@
 namespace MetroGid.Controllers.DTO;
 
-public class StationDTO(string title, int occupancy, AccessTypeDTO type, TimeOnly open_time, TimeOnly close_time)
+public class StationDTO(
+    string title, int occupancy, AccessTypeDTO type,
+    TimeOnly open_time, TimeOnly close_time
+)
 {
     public string Title { get; set; } = title;
     public int Occupancy { get; set; } = occupancy;
     public AccessTypeDTO Type { get; set; } = type;
     public TimeOnly OpenTime { get; set; } = open_time;
     public TimeOnly CloseTime { get; set; } = close_time;
+
+
+    public override bool Equals(object? obj) =>
+        obj is StationDTO other &&
+        Title == other.Title &&
+        Occupancy == other.Occupancy &&
+        Type == other.Type &&
+        OpenTime == other.OpenTime &&
+        CloseTime == other.CloseTime;
+
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Title, Occupancy, Type, OpenTime, CloseTime);
 }
