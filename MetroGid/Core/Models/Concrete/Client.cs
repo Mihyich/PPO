a47e6148214b1e92cc9e1412dbc020773012af84
GrizlyBear <michail.zevahin@gmail.com>
2025-05-03 10:47:58 +0300

    Доменная сущность Client поддерживает визитерную валидацию

--- MetroGid/Core/Models/Concrete/Client.cs
+++ MetroGid/Core/Models/Concrete/Client.cs
@@ -1,11 +1,17 @@
 using MetroGid.Core.Models.Types;
+using MetroGid.Core.Utilities.Validators.Interfaces;
 
 namespace MetroGid.Core.Models.Concrete;
 
-public class Client(string login, string password, string mail, RoleType role = RoleType.UNSIGNED)
+public class Client(
+    string login, string password, string mail,
+    RoleType role = RoleType.UNSIGNED
+) : IDomainValidatorAccepter
 {
     public string Login { get; set; } = login;
     public string Password { get; set; } = password;
     public string Mail { get; set; } = mail;
     public RoleType Role { get; set; } = role;
+
+    public void Validate(IDomainValidatorVisitor visitor) => visitor.Visit(this);
 }
