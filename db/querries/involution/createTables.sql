37ed4a456f1d824f5ed62815709b256027aef376
GrizlyBear <michail.zevahin@gmail.com>
2025-05-09 21:59:57 +0300

    NOT NULL проверки вынесены в отдельный файл

--- db/querries/involution/createTables.sql
+++ db/querries/involution/createTables.sql
@@ -5,29 +5,29 @@ CREATE TABLE IF NOT EXISTS client (
 	client_login	VARCHAR(255),
 	client_password	VARCHAR(255),
 	mail			VARCHAR(255),
-	privilege		role_type NOT NULL
+	privilege		role_type
 );
 
 CREATE TABLE IF NOT EXISTS chart (
 	id				SERIAL PRIMARY KEY,
 	city			VARCHAR(255),
 	title			VARCHAR(255),
-	svg_content		XML NOT NULL
+	svg_content		XML
 );
 
 CREATE TABLE IF NOT EXISTS branch (
 	id				SERIAL PRIMARY KEY,
 	title			VARCHAR(255),
-	color			decimal_hexcolor NOT NULL,
-	access			access_type NOT NULL
+	color			decimal_hexcolor,
+	access			access_type
 );
 
 CREATE TABLE IF NOT EXISTS station (
 	id				SERIAL PRIMARY KEY,
 	duty_id			INT,
 	title			VARCHAR(255),
-	occupancy		occupancy_level NOT NULL,
-	access			access_type NOT NULL,
+	occupancy		occupancy_level,
+	access			access_type,
 	open_time		TIME,
 	close_time		TIME,
 
@@ -37,8 +37,8 @@ CREATE TABLE IF NOT EXISTS station (
 CREATE TABLE IF NOT EXISTS transition (
 	id				SERIAL PRIMARY KEY,
 	duty_id			INT,
-	occupancy		occupancy_level NOT NULL,
-	access			access_type NOT NULL,
+	occupancy		occupancy_level,
+	access			access_type,
 	duration		TIME,
 	open_time		TIME,
 	close_time		TIME,
@@ -48,8 +48,8 @@ CREATE TABLE IF NOT EXISTS transition (
 
 CREATE TABLE IF NOT EXISTS way (
 	id				SERIAL PRIMARY KEY,
-	client_id		INT NOT NULL,
-	chart_id		INT NOT NULL,
+	client_id		INT,
+	chart_id		INT,
 	title			VARCHAR(255),
 	init_date		TIMESTAMP,
 
@@ -60,10 +60,10 @@ CREATE TABLE IF NOT EXISTS way (
 
 CREATE TABLE IF NOT EXISTS way_item (
 	id				SERIAL PRIMARY KEY,
-	way_id			INT NOT NULL,
-	nexus			nexus_type NOT NULL,
+	way_id			INT,
+	nexus			nexus_type,
 	step_nomer		INT,
-	
+
 	FOREIGN KEY (way_id) REFERENCES way (id),
 	UNIQUE (way_id, step_nomer)
 );
@@ -72,8 +72,8 @@ CREATE TABLE IF NOT EXISTS way_item (
 
 CREATE TABLE IF NOT EXISTS chart_branch (
 	id				SERIAL PRIMARY KEY,
-	chart_id		INT NOT NULL,
-	branch_id		INT NOT NULL,
+	chart_id		INT,
+	branch_id		INT,
 
 	FOREIGN KEY (chart_id) REFERENCES chart (id),
 	FOREIGN KEY (branch_id) REFERENCES branch (id),
@@ -82,8 +82,8 @@ CREATE TABLE IF NOT EXISTS chart_branch (
 
 CREATE TABLE IF NOT EXISTS branch_station (
 	id				SERIAL PRIMARY KEY,
-	branch_id		INT NOT NULL,
-	station_id		INT NOT NULL,
+	branch_id		INT,
+	station_id		INT,
 
 	FOREIGN KEY (branch_id) REFERENCES branch (id),
 	FOREIGN KEY (station_id) REFERENCES station (id),
@@ -92,8 +92,8 @@ CREATE TABLE IF NOT EXISTS branch_station (
 
 CREATE TABLE IF NOT EXISTS railway (
 	id				SERIAL PRIMARY KEY,
-	from_id			INT NOT NULL,
-	to_id			INT NOT NULL,
+	from_id			INT,
+	to_id			INT,
 	duration		TIME,
 
 	FOREIGN KEY (from_id) REFERENCES station (id),
@@ -104,8 +104,8 @@ CREATE TABLE IF NOT EXISTS railway (
 
 CREATE TABLE IF NOT EXISTS station_transition (
 	id				SERIAL PRIMARY KEY,
-	station_id		INT NOT NULL,
-	transition_id	INT NOT NULL,
+	station_id		INT,
+	transition_id	INT,
 
 	FOREIGN KEY (station_id) REFERENCES station (id),
 	FOREIGN KEY (transition_id) REFERENCES transition (id)
@@ -113,8 +113,8 @@ CREATE TABLE IF NOT EXISTS station_transition (
 
 CREATE TABLE IF NOT EXISTS way_item_station (
 	id				SERIAL PRIMARY KEY,
-	station_id		INT NOT NULL,
-	wayitem_id		INT NOT NULL,
+	station_id		INT,
+	wayitem_id		INT,
 
 	FOREIGN KEY (station_id) REFERENCES station (id),
 	FOREIGN KEY (wayitem_id) REFERENCES way_item (id),
@@ -124,8 +124,8 @@ CREATE TABLE IF NOT EXISTS way_item_station (
 
 CREATE TABLE IF NOT EXISTS way_item_railway (
 	id				SERIAL PRIMARY KEY,
-	railway_id		INT NOT NULL,
-	wayitem_id		INT NOT NULL,
+	railway_id		INT,
+	wayitem_id		INT,
 
 	FOREIGN KEY (railway_id) REFERENCES railway (id),
 	FOREIGN KEY (wayitem_id) REFERENCES way_item (id),
@@ -135,8 +135,8 @@ CREATE TABLE IF NOT EXISTS way_item_railway (
 
 CREATE TABLE IF NOT EXISTS way_item_transition (
 	id				SERIAL PRIMARY KEY,
-	transition_id	INT NOT NULL,
-	wayitem_id		INT NOT NULL,
+	transition_id	INT,
+	wayitem_id		INT,
 	
 	FOREIGN KEY (transition_id) REFERENCES transition (id),
 	FOREIGN KEY (wayitem_id) REFERENCES way_item (id),
