770a20c23cf1d5059102828359040efb5f41329b
GrizlyBear <michail.zevahin@gmail.com>
2025-05-09 19:09:17 +0300

    Добавлен вывод статистики о доменных тапах данных

--- db/querries/meta_data.sql
+++ db/querries/meta_data.sql
@@ -1,19 +1,39 @@
 -- Просмотр пользовательских ENUM
 SELECT 
     n.nspname AS "Schema",
-    p.typname AS "Type Name",
-    pg_catalog.format_type(p.typbasetype, NULL) AS "Base Type",
-    (SELECT array_agg(e.enumlabel) 
-     FROM pg_enum e 
-     WHERE e.enumtypid = p.oid) AS "Enum Values"
+    t.typname AS "Type Name",
+    (SELECT
+        array_agg(e.enumlabel) 
+    FROM
+        pg_enum AS e 
+    WHERE
+        e.enumtypid = t.oid
+    ) AS "Enum Values",
+    t.typnotnull AS "Not Null"
 FROM 
-    pg_catalog.pg_type p
+    pg_catalog.pg_type AS t
 JOIN 
-    pg_catalog.pg_namespace n ON n.oid = p.typnamespace
+    pg_catalog.pg_namespace AS n ON n.oid = t.typnamespace
 WHERE 
-    p.typtype = 'e' -- 'e' для ENUM
+    t.typtype = 'e' AND n.nspname = 'public' -- 'e' для ENUM
 ORDER BY 
     "Schema", "Type Name";
+
+-- Просмотр пользовательских DOMAIN-типов
+SELECT 
+    n.nspname AS "Schema",
+    t.typname AS "Domain Name",
+    t.typnotnull AS "Not Null"
+FROM 
+    pg_catalog.pg_type AS t
+JOIN 
+    pg_catalog.pg_namespace AS n ON n.oid = t.typnamespace
+LEFT JOIN 
+    pg_catalog.pg_constraint AS c ON c.contypid = t.oid
+WHERE 
+    t.typtype = 'd' AND n.nspname = 'public' -- 'd' для DOMAIN
+ORDER BY 
+    "Schema", "Domain Name";
 
 
 -- Просмотр ролей
