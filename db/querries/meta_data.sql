304a62db8365ef7b25571ec6b3af63b8b9b25a62
GrizlyBear <michail.zevahin@gmail.com>
2025-05-09 19:32:54 +0300

    Добавлен просмотр пользовательских функций

--- db/querries/meta_data.sql
+++ db/querries/meta_data.sql
@@ -1,4 +1,4 @@
--- Просмотр пользовательских ENUM
+-- ENUM-типы
 SELECT 
     n.nspname AS "Schema",
     t.typname AS "Type Name",
@@ -19,7 +19,7 @@ WHERE
 ORDER BY 
     "Schema", "Type Name";
 
--- Просмотр пользовательских DOMAIN-типов
+-- DOMAIN-типы
 SELECT 
     n.nspname AS "Schema",
     t.typname AS "Domain Name",
@@ -35,6 +35,30 @@ WHERE
 ORDER BY 
     "Schema", "Domain Name";
 
+-- Функции
+SELECT 
+    n.nspname AS "Schema",
+    p.proname AS "Function Name",
+    pg_catalog.pg_get_function_result(p.oid) AS "Return Type",
+    pg_catalog.pg_get_function_arguments(p.oid) AS "Arguments",
+    CASE 
+        WHEN p.prokind = 'a' THEN 'Aggregate'
+        WHEN p.prokind = 'w' THEN 'Window'
+        WHEN p.prokind = 'f' THEN 'Function'
+        WHEN p.prokind = 'p' THEN 'Procedure'
+    END AS "Type",
+    l.lanname AS "Language"
+FROM 
+    pg_catalog.pg_proc p
+LEFT JOIN 
+    pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+LEFT JOIN 
+    pg_catalog.pg_language l ON l.oid = p.prolang
+WHERE 
+    n.nspname NOT IN ('pg_catalog', 'information_schema')
+    AND n.nspname NOT LIKE 'pg_%'
+ORDER BY 
+    "Schema", "Function Name";
 
--- Просмотр ролей
+-- Роли
 SELECT * FROM pg_user;
