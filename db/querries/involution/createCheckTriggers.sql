7f4d12e3744df572243ec94f19ec8a8412bd21f4
GrizlyBear <michail.zevahin@gmail.com>
2025-05-10 11:36:12 +0300

    Создана триггерная функция проверки единственности владельцев -- веток у станций

--- db/querries/involution/createCheckTriggers.sql
+++ db/querries/involution/createCheckTriggers.sql
@@ -18,6 +18,26 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION ck_branch_station_unique_ref() RETURNS TRIGGER
+AS $$
+BEGIN
+    IF EXISTS (
+        SELECT
+            1
+        FROM
+            branch_station
+        WHERE
+            branch_id <> NEW.branch_id AND station_id = NEW.station_id
+    ) THEN
+        RAISE EXCEPTION
+            'Попытка связать станцию (%), уже связанную с веткой (%), с веткой (%)',
+            station_id, branch_id, NEW.branch_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION ck_railway_unique_station_ref() RETURNS TRIGGER
 AS $$
 BEGIN
