4f6e5cbb070c65c5f63256d0f70d82eaf360fb82
GrizlyBear <michail.zevahin@gmail.com>
2025-05-11 11:22:36 +0300

    Упразднение создания ролей

--- db/querries/involution/createRoles.sql
+++ db/querries/involution/createRoles.sql
@@ -1,20 +1,3 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_roles WHERE rolname = 'unsigned_client'
-    ) THEN
-        CREATE ROLE unsigned_client WITH LOGIN PASSWORD 'unsigned_client';
-    END IF;
-
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_roles WHERE rolname = 'signed_client'
-    ) THEN
-        CREATE ROLE signed_client WITH LOGIN PASSWORD 'signed_client';
-    END IF;
-
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_roles WHERE rolname = 'duty'
-    ) THEN
-        CREATE ROLE duty WITH LOGIN PASSWORD 'duty';
-    END IF;
-END $$;
+CREATE ROLE unsigned_client WITH LOGIN PASSWORD 'unsigned_client';
+CREATE ROLE signed_client WITH LOGIN PASSWORD 'signed_client';
+CREATE ROLE duty WITH LOGIN PASSWORD 'duty';
