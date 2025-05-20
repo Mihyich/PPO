02315691325da19e97259fbb935469328f00f201
GrizlyBear <michail.zevahin@gmail.com>
2025-05-20 16:54:43 +0300

    Создано удаление триггерных функций

---
+++ db/querries/convolution/dropCheckTriggers.sql
@@ -0,0 +1,6 @@
+DROP FUNCTION IF EXISTS 
+    ck_chart_branch_unique_ref,
+    ck_branch_station_unique_ref,
+    ck_railway_unique_station_ref,
+    ck_railway_same_branch_of_stations_ref,
+    ck_station_transition_different_branch_of_stations_ref;
