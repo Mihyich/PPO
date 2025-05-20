6dc61a78eccd57e269ccf44cfbcb996aaf87e6dd
GrizlyBear <michail.zevahin@gmail.com>
2025-05-20 17:00:19 +0300

    Связывание внешних ключей вынесено в файл ограничений && Настройка поведения при удалении

--- db/querries/involution/createConstraits.sql
+++ db/querries/involution/createConstraits.sql
@@ -9,8 +9,10 @@ ALTER TABLE chart
 	ALTER COLUMN city SET NOT NULL,
 	ALTER COLUMN title SET NOT NULL,
 	ALTER COLUMN svg_content SET DEFAULT '<svg width="200" height="200"><text y="16">Пустая схема</text></svg>',
-	ADD CONSTRAINT ck_valid_svg CHECK (svg_content::xml IS DOCUMENT),
-	ADD CONSTRAINT uk_chart_city_title UNIQUE (city, title); -- Не должно быть такого!!!
+	ADD CONSTRAINT ck_valid_svg
+		CHECK (svg_content::xml IS DOCUMENT),
+	ADD CONSTRAINT uk_chart_city_title
+		UNIQUE (city, title); -- Не должно быть такого!!!
 
 
 ALTER TABLE branch
@@ -20,21 +22,29 @@ ALTER TABLE branch
 
 
 ALTER TABLE station
+	ALTER COLUMN duty_id DROP NOT NULL,
 	ALTER COLUMN title SET NOT NULL,
 	ALTER COLUMN occupancy SET DEFAULT 5,
 	ALTER COLUMN access SET DEFAULT 'ACCESSIBLE',
 	ALTER COLUMN open_time SET NOT NULL,
 	ALTER COLUMN close_time SET NOT NULL,
-	ADD CONSTRAINT ck_station_different_open_close_time CHECK (open_time <> close_time);
+	ADD CONSTRAINT ck_station_different_open_close_time
+		CHECK (open_time <> close_time),
+	ADD CONSTRAINT fk_station_duty_id
+		FOREIGN KEY (duty_id) REFERENCES client (id) ON DELETE SET NULL;
 
 
 ALTER TABLE transition
+	ALTER COLUMN duty_id DROP NOT NULL,
 	ALTER COLUMN occupancy SET DEFAULT 5,
 	ALTER COLUMN access SET DEFAULT 'ACCESSIBLE',
 	ALTER COLUMN duration SET NOT NULL,
 	ALTER COLUMN open_time SET NOT NULL,
 	ALTER COLUMN close_time SET NOT NULL,
-	ADD CONSTRAINT ck_transition_different_open_close_time CHECK (open_time <> close_time);
+	ADD CONSTRAINT ck_transition_different_open_close_time
+		CHECK (open_time <> close_time),
+	ADD CONSTRAINT fk_transition_duty_id
+		FOREIGN KEY (duty_id) REFERENCES client (id) ON DELETE SET NULL;
 
 
 ALTER TABLE way
@@ -42,60 +52,109 @@ ALTER TABLE way
 	ALTER COLUMN chart_id SET NOT NULL,
 	ALTER COLUMN title SET NOT NULL,
 	ALTER COLUMN init_date SET NOT NULL,
-	ADD CONSTRAINT uk_way_client_id_title UNIQUE (client_id, title);
+	ADD CONSTRAINT uk_way_client_id_title
+		UNIQUE (client_id, title),
+	ADD CONSTRAINT fk_way_client_id
+		FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_way_chart_id
+		FOREIGN KEY (chart_id) REFERENCES chart (id) ON DELETE CASCADE;
 
 
 ALTER TABLE way_item
 	ALTER COLUMN way_id SET NOT NULL,
 	ALTER COLUMN nexus SET NOT NULL,
 	ALTER COLUMN step_nomer SET NOT NULL,
-	ADD CONSTRAINT uk_way_item_way_id_step_nomer UNIQUE (way_id, step_nomer);
+	ADD CONSTRAINT uk_way_item_way_id_step_nomer
+		UNIQUE (way_id, step_nomer),
+	ADD CONSTRAINT fk_way_item_way_id
+		FOREIGN KEY (way_id) REFERENCES way (id) ON DELETE CASCADE;
 
 
 ALTER TABLE chart_branch
 	ALTER COLUMN chart_id SET NOT NULL,
 	ALTER COLUMN branch_id SET NOT NULL,
-	ADD CONSTRAINT uk_chart_branch_chart_id_branch_id UNIQUE (chart_id, branch_id), -- Ни одна схема не имеет дубликатов веток
-	ADD CONSTRAINT uk_chart_branch_branch_id UNIQUE (branch_id); -- Каждая ветка связывается со схемой только один раз
+	ADD CONSTRAINT uk_chart_branch_chart_id_branch_id
+		UNIQUE (chart_id, branch_id), -- Ни одна схема не имеет дубликатов веток
+	ADD CONSTRAINT uk_chart_branch_branch_id
+		UNIQUE (branch_id), -- Каждая ветка связывается со схемой только один раз
+	ADD CONSTRAINT fk_chart_branch_chart_id
+		FOREIGN KEY (chart_id) REFERENCES chart (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_chart_branch_branch_id
+		FOREIGN KEY (branch_id) REFERENCES branch (id) ON DELETE CASCADE;
 
 
 ALTER TABLE branch_station
 	ALTER COLUMN branch_id SET NOT NULL,
 	ALTER COLUMN station_id SET NOT NULL,
-	ADD CONSTRAINT uk_branch_station_branch_id_station_id UNIQUE (branch_id, station_id), -- Ни одна ветка не имеет дубликатов станций
-	ADD CONSTRAINT uk_branch_station_station_id UNIQUE (station_id); -- Каждая станция связывается с веткой только один раз
+	ADD CONSTRAINT uk_branch_station_branch_id_station_id
+		UNIQUE (branch_id, station_id), -- Ни одна ветка не имеет дубликатов станций
+	ADD CONSTRAINT uk_branch_station_station_id
+		UNIQUE (station_id), -- Каждая станция связывается с веткой только один раз
+	ADD CONSTRAINT fk_branch_station_branch_id
+		FOREIGN KEY (branch_id) REFERENCES branch (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_branch_station_station_id
+		FOREIGN KEY (station_id) REFERENCES station (id) ON DELETE CASCADE;
 
 
 ALTER TABLE railway
 	ALTER COLUMN from_id SET NOT NULL,
 	ALTER COLUMN to_id SET NOT NULL,
 	ALTER COLUMN duration SET NOT NULL,
-	ADD CONSTRAINT uk_railway_from_id_to_id UNIQUE (from_id, to_id),
-	ADD CONSTRAINT ck_railway_different_stations CHECK (from_id <> to_id);
+	ADD CONSTRAINT uk_railway_from_id_to_id
+		UNIQUE (from_id, to_id),
+	ADD CONSTRAINT ck_railway_different_stations
+		CHECK (from_id <> to_id),
+	ADD CONSTRAINT fk_railway_from_id
+		FOREIGN KEY (from_id) REFERENCES station (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_railway_to_id
+		FOREIGN KEY (to_id) REFERENCES station (id) ON DELETE CASCADE;
 
 
 ALTER TABLE station_transition
 	ALTER COLUMN station_id SET NOT NULL,
 	ALTER COLUMN transition_id SET NOT NULL,
-	ADD CONSTRAINT uk_station_transition_station_id_transition_id UNIQUE (station_id, transition_id);
+	ADD CONSTRAINT uk_station_transition_station_id_transition_id
+		UNIQUE (station_id, transition_id),
+	ADD CONSTRAINT fk_station_transition_station_id
+		FOREIGN KEY (station_id) REFERENCES station (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_station_transition_transition_id
+		FOREIGN KEY (transition_id) REFERENCES transition (id) ON DELETE CASCADE;
 
 
 ALTER TABLE way_item_station
 	ALTER COLUMN way_item_id SET NOT NULL,
 	ALTER COLUMN station_id SET NOT NULL,
-	ADD CONSTRAINT uk_way_item_station_way_item_id_station_id UNIQUE (way_item_id, station_id),
-	ADD CONSTRAINT uk_way_item_station_way_item_id UNIQUE (way_item_id);
+	ADD CONSTRAINT uk_way_item_station_way_item_id_station_id
+		UNIQUE (way_item_id, station_id),
+	ADD CONSTRAINT uk_way_item_station_way_item_id
+		UNIQUE (way_item_id),
+	ADD CONSTRAINT fk_way_item_station_way_item_id
+		FOREIGN KEY (way_item_id) REFERENCES way_item (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_way_item_station_station_id
+		FOREIGN KEY (station_id) REFERENCES station (id) ON DELETE CASCADE;
 
 
 ALTER TABLE way_item_railway
 	ALTER COLUMN way_item_id SET NOT NULL,
 	ALTER COLUMN railway_id SET NOT NULL,
-	ADD CONSTRAINT uk_way_item_railway_way_item_id_railway_id UNIQUE (way_item_id, railway_id),
-	ADD CONSTRAINT uk_way_item_railway_way_item_id UNIQUE (way_item_id);
+	ADD CONSTRAINT uk_way_item_railway_way_item_id_railway_id
+		UNIQUE (way_item_id, railway_id),
+	ADD CONSTRAINT uk_way_item_railway_way_item_id
+		UNIQUE (way_item_id),
+	ADD CONSTRAINT fk_way_item_railway_way_item_id
+		FOREIGN KEY (way_item_id) REFERENCES way_item (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_way_item_railway_railway_id
+		FOREIGN KEY (railway_id) REFERENCES railway (id) ON DELETE CASCADE;
 
 
 ALTER TABLE way_item_transition
 	ALTER COLUMN way_item_id SET NOT NULL,
 	ALTER COLUMN transition_id SET NOT NULL,
-	ADD CONSTRAINT uk_way_item_transition_way_item_id_transition_id UNIQUE (way_item_id, transition_id),
-	ADD CONSTRAINT uk_way_item_transition_way_item_id UNIQUE (way_item_id);
+	ADD CONSTRAINT uk_way_item_transition_way_item_id_transition_id
+		UNIQUE (way_item_id, transition_id),
+	ADD CONSTRAINT uk_way_item_transition_way_item_id
+		UNIQUE (way_item_id),
+	ADD CONSTRAINT fk_way_item_transition_way_item_id
+		FOREIGN KEY (way_item_id) REFERENCES way_item (id) ON DELETE CASCADE,
+	ADD CONSTRAINT fk_way_item_transition_transition_id
+		FOREIGN KEY (transition_id) REFERENCES transition (id) ON DELETE CASCADE;
--- db/querries/involution/createTables.sql
+++ db/querries/involution/createTables.sql
@@ -29,9 +29,7 @@ CREATE TABLE IF NOT EXISTS station (
 	occupancy       occupancy_level,
 	access          access_type,
 	open_time       TIME,
-	close_time      TIME,
-
-	FOREIGN KEY (duty_id) REFERENCES client (id)
+	close_time      TIME
 );
 
 CREATE TABLE IF NOT EXISTS transition (
@@ -41,9 +39,7 @@ CREATE TABLE IF NOT EXISTS transition (
 	access          access_type,
 	duration        TIME,
 	open_time       TIME,
-	close_time      TIME,
-
-	FOREIGN KEY (duty_id) REFERENCES client (id)
+	close_time      TIME
 );
 
 CREATE TABLE IF NOT EXISTS way (
@@ -51,19 +47,14 @@ CREATE TABLE IF NOT EXISTS way (
 	client_id       INT,
 	chart_id        INT,
 	title           VARCHAR(255),
-	init_date       TIMESTAMP,
-
-	FOREIGN KEY (client_id) REFERENCES client (id),
-	FOREIGN KEY (chart_id) REFERENCES chart (id)
+	init_date       TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS way_item (
 	id              SERIAL PRIMARY KEY,
 	way_id          INT,
 	nexus           nexus_type, -- Тип звена маршрута [станция | переход | переезд]
-	step_nomer      INT,
-
-	FOREIGN KEY (way_id) REFERENCES way (id)
+	step_nomer      INT
 );
 
 -- Связующие таблицы
@@ -71,63 +62,42 @@ CREATE TABLE IF NOT EXISTS way_item (
 CREATE TABLE IF NOT EXISTS chart_branch (
 	id              SERIAL PRIMARY KEY,
 	chart_id        INT,
-	branch_id       INT,
-
-	FOREIGN KEY (chart_id) REFERENCES chart (id),
-	FOREIGN KEY (branch_id) REFERENCES branch (id)
+	branch_id       INT
 );
 
 CREATE TABLE IF NOT EXISTS branch_station (
 	id              SERIAL PRIMARY KEY,
 	branch_id       INT,
-	station_id      INT,
-
-	FOREIGN KEY (branch_id) REFERENCES branch (id),
-	FOREIGN KEY (station_id) REFERENCES station (id)
+	station_id      INT
 );
 
 CREATE TABLE IF NOT EXISTS railway (
 	id              SERIAL PRIMARY KEY,
 	from_id         INT,
 	to_id           INT,
-	duration        TIME,
-
-	FOREIGN KEY (from_id) REFERENCES station (id),
-	FOREIGN KEY (to_id) REFERENCES station (id)
+	duration        TIME
 );
 
 CREATE TABLE IF NOT EXISTS station_transition (
 	id              SERIAL PRIMARY KEY,
 	station_id      INT,
-	transition_id   INT,
-
-	FOREIGN KEY (station_id) REFERENCES station (id),
-	FOREIGN KEY (transition_id) REFERENCES transition (id)
+	transition_id   INT
 );
 
 CREATE TABLE IF NOT EXISTS way_item_station (
 	id              SERIAL PRIMARY KEY,
 	way_item_id     INT,
-	station_id      INT,
-
-	FOREIGN KEY (station_id) REFERENCES station (id),
-	FOREIGN KEY (way_item_id) REFERENCES way_item (id)
+	station_id      INT
 );
 
 CREATE TABLE IF NOT EXISTS way_item_railway (
 	id              SERIAL PRIMARY KEY,
 	way_item_id     INT,
-	railway_id      INT,
-
-	FOREIGN KEY (railway_id) REFERENCES railway (id),
-	FOREIGN KEY (way_item_id) REFERENCES way_item (id)
+	railway_id      INT
 );
 
 CREATE TABLE IF NOT EXISTS way_item_transition (
 	id              SERIAL PRIMARY KEY,
 	way_item_id     INT,
-	transition_id   INT,
-	
-	FOREIGN KEY (transition_id) REFERENCES transition (id),
-	FOREIGN KEY (way_item_id) REFERENCES way_item (id)
+	transition_id   INT
 );
