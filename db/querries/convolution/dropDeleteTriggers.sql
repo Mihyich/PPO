ec32db921360c3e6ff92a9f0123b2e5c8d8b5cb2
GrizlyBear <michail.zevahin@gmail.com>
2025-05-21 03:52:42 +0300

    Удаление удаляющих триггерных функций

---
+++ db/querries/convolution/dropDeleteTriggers.sql
@@ -0,0 +1,4 @@
+DROP FUNCTION IF EXISTS 
+    delete_orphaned_branches_before_chart_delete,
+    delete_orphaned_stations_before_branch_delete,
+    delete_orphaned_transitions_before_station_delete;
