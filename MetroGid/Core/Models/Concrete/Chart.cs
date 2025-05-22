5cc2bca7063621d30a9c34bf2e8fe4ca639ee396
GrizlyBear <michail.zevahin@gmail.com>
2025-05-22 17:58:52 +0300

    Укорочен метод Chart - GetStation.

--- MetroGid/Core/Models/Concrete/Chart.cs
+++ MetroGid/Core/Models/Concrete/Chart.cs
@@ -10,16 +10,10 @@ public class Chart(string title, string city, string svg_inst) : IDomainValidato
     public string SvgInst { get; set; } = svg_inst;
     public List<Branch> Branches { get; set; } = [];
 
-    public Station? GetStation(string branch_title, string station_title)
-    {
-        Branch? branch = null;
-        Station? station = null;
-
-        if ((branch = Branches.FirstOrDefault(b => b.Title == branch_title)) != null)
-            station = branch.Stations.FirstOrDefault(s => s.Title == station_title);
-
-        return station;
-    }
+    public Station? GetStation(string branchTitle, string stationTitle) =>
+        Branches
+            .FirstOrDefault(b => b.Title == branchTitle)?.Stations?
+                .FirstOrDefault(s => s.Title == stationTitle);
 
     public Route Search(Station src, Station dst, TimeOnly timeStart, StrategySearchRouteBase Searcher)
     {
