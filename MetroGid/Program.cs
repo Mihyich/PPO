83ec81df2733a68d533beaa9d512a3580bf4d7a2
GrizlyBear <michail.zevahin@gmail.com>
2025-05-22 16:08:37 +0300

    Гойда, Йоу

--- MetroGid/Program.cs
+++ MetroGid/Program.cs
@@ -10,6 +10,12 @@ using MetroGid.Core.Utilities.Strategies;
 using MetroGid.Core.Utilities.Validators.Handlers;
 using MetroGid.Core.Utilities.Validators.Interfaces;
 
+using MetroGid.Core.Services;
+using MetroGid.Core.Interfaces;
+using MetroGid.DBA.EF.Repositories;
+using MetroGid.DBA.EF.Context;
+using MetroGid.DBA.EF.Converters;
+
 class Program
 {
     static void Main(string[] args)
@@ -21,8 +27,9 @@ class Program
         IDomainValidatorVisitor domainReferentialityValidator = new ThrowableDomainReferentialityValidator(handler, logger);
 
         BuilderChart builder = new(domainAttribsValidator, domainReferentialityValidator);
+        // DirectorChartJson director = new(builder, FileReader.ReadAll("/home/mihail/Рабочий стол/BMSTU/PPO_BACKUP/cities/Adana/chart.json"));
         // DirectorChartJson director = new(builder, FileReader.ReadAll("/home/mihail/Рабочий стол/BMSTU/PPO/temp_cities/Sankt-Peterburg/init.json"));
-        DirectorChartJson director = new(builder, FileReader.ReadAll("/home/mihail/Рабочий стол/BMSTU/PPO/cities/Moscow/chart.json"));
+        DirectorChartJson director = new(builder, FileReader.ReadAll("/home/mihail/Рабочий стол/BMSTU/PPO_BACKUP/cities/Moscow/chart.json"));
         Chart chart = director.Construct();
 
         chart.Searcher = new StrategySearchRouteBFS();
@@ -41,10 +48,39 @@ class Program
             route?.Validate(domainAttribsValidator);
             route?.Validate(domainReferentialityValidator);
             route?.Output();
+
+            string jsonContent = DomainModelConverter.Convert(route!);
+            File.WriteAllText("/home/mihail/Рабочий стол/BMSTU/PPO/route.json", jsonContent);
         }
         else
         {
             Console.WriteLine($"Не все целевые станции были найдены");
         }
+
+
+
+        // MetroContext metroContext = new();
+        // IClientRepository clientRepository = new EFClientRepository(metroContext);
+        // ClientService clientService = new(clientRepository, handler, logger);
+
+        // int clientId1 = clientService.Reg("mihail", "qwerASDF1234", "michail.zevahin@gmail.com").GetAwaiter().GetResult();
+        // Console.WriteLine($"Айди нового пользователя {clientId1}");
+
+        // int clientId2 = clientService.Reg("GrizlyBear", "GrizlyBearGoyda2004", "berloga.taiga@gmail.com").GetAwaiter().GetResult();
+        // Console.WriteLine($"Айди нового пользователя {clientId2}");
+
+        // int clientId1 = 1;
+        // int clientId2 = 2;
+        // int stationId = 1;
+        // int transitionId = 1;
+
+        // int dutyId = clientRepository.GetStationDuty(stationId).GetAwaiter().GetResult();
+        // Console.WriteLine($"Айди дежурного у станции {stationId}: {dutyId}");
+
+        // int changes = clientRepository.MakeDuty(clientId1, stationId, transitionId).GetAwaiter().GetResult();
+        // Console.WriteLine($"Проделанные изменения: {changes}");
+
+        // dutyId = clientRepository.GetStationDuty(stationId).GetAwaiter().GetResult();
+        // Console.WriteLine($"Айди дежурного у станции {stationId}: {dutyId}");
     }
 }
