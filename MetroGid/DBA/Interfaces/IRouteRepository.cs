1281dca91ec778b4807acb0d77c9830df49139dc
GrizlyBear <michail.zevahin@gmail.com>
2025-05-23 00:35:50 +0300

    Исключена возможность возвврата null ссылок из методов репозитория Route

--- MetroGid/DBA/Interfaces/IRouteRepository.cs
+++ MetroGid/DBA/Interfaces/IRouteRepository.cs
@@ -7,11 +7,11 @@ public interface IRouteRepository
     Task<int> AddAsync(Route route, int clientId, int chartId);
 
     Task<int> GetIdAsync(string title, int clientId);
-    Task<Route?> GetByIdAsync(int id);
-    Task<List<string>?> GetAllTitlesForClientAsync(int clientId);
-    Task<List<string>?> GetAllTitlesForClientOfChartAsync(int clientId, int chartId);
-    Task<List<Route>?> GetAllForClientIdAsync(int clientId);
-    Task<List<Route>?> GetAllForClientOfChartIdAsync(int clientId, int chartId);
+    Task<Route> GetByIdAsync(int id);
+    Task<List<string>> GetAllTitlesForClientAsync(int clientId);
+    Task<List<string>> GetAllTitlesForClientOfChartAsync(int clientId, int chartId);
+    Task<List<Route>> GetAllForClientIdAsync(int clientId);
+    Task<List<Route>> GetAllForClientOfChartIdAsync(int clientId, int chartId);
 
     Task UpdateAsync(int id, Route route);
 
