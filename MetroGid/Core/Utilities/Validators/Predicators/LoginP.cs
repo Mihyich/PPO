3514aff7a9fd720910e9d087916fd495deb33650
GrizlyBear <michail.zevahin@gmail.com>
2025-08-14 13:27:31 +0300

    Обновлены предикаты

--- MetroGid/Core/Utilities/Validators/Predicators/LoginP.cs
+++ MetroGid/Core/Utilities/Validators/Predicators/LoginP.cs
@@ -2,7 +2,7 @@ namespace MetroGid.Core.Utilities.Validators.Predicators;
 
 public static class LoginP
 {
-    public static readonly int MinLength = 4;
+    public static readonly int MinLength = 1;
     public static readonly int MaxLength = 255;
 
     public static bool IsOutOfRange(string l) =>
--- MetroGid/Core/Utilities/Validators/Predicators/PasswordP.cs
+++ MetroGid/Core/Utilities/Validators/Predicators/PasswordP.cs
@@ -2,7 +2,7 @@ namespace MetroGid.Core.Utilities.Validators.Predicators;
 
 public static class PasswordP
 {
-    public static readonly int MinLength = 4;
+    public static readonly int MinLength = 6;
     public static readonly int MaxLength = 255;
 
     public static bool IsOutOfRange(string p) =>
