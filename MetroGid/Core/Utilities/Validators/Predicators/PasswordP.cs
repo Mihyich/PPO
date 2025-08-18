d278cc15ea459a0a23407f0b08401083fb6c24b1
GrizlyBear <michail.zevahin@gmail.com>
2025-08-18 15:44:45 +0300

    Обновлены предикаторы

--- MetroGid/Core/Utilities/Validators/Predicators/PasswordP.cs
+++ MetroGid/Core/Utilities/Validators/Predicators/PasswordP.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace MetroGid.Core.Utilities.Validators.Predicators;
 
 public static class PasswordP
@@ -8,4 +10,20 @@ public static class PasswordP
     public static bool IsOutOfRange(string p) =>
         p.Length < MinLength || p.Length > MaxLength;
 
+    public static bool IsValid(string p)
+    {
+        if (!Regex.IsMatch(p, @"[A-ZА-Я]"))
+            return false;
+
+        if (!Regex.IsMatch(p, @"[a-zа-я]"))
+            return false;
+
+        if (!Regex.IsMatch(p, @"\d"))
+            return false;
+
+        if (!Regex.IsMatch(p, @"^[A-ZА-Яa-zа-я0-9_!@#$%^&*()\-+=\[\]{}|:;,.?<>~`""]+$"))
+            return false;
+
+        return true;
+    }
 }
