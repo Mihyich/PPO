34b634c2b7db29530cb975966d4b83b7c27a4caf
GrizlyBear <michail.zevahin@gmail.com>
2025-08-19 10:21:56 +0300

    Скорректированы типы возвращаемых переменных

--- MetroGid/Core/Interfaces/IChartRepository.cs
+++ MetroGid/Core/Interfaces/IChartRepository.cs
@@ -6,18 +6,18 @@ public interface IChartRepository
 {
     Task<int> AddAsync(Chart chart);
 
-    Task<string> GetChartJsonByIdAsync(int chartId);
-    Task<Chart> GetChartWeakByIdAsync(int chartId);
-    Task<Branch> GetBranchWeakByIdAsync(int branchId);
-    Task<Station> GetStationWeakByIdAsync(int stationId);
-    Task<Railway> GetRailwayByIdAsync(int railwayId);
-    Task<Transition> GetTransitionByIdAsync(int transitionId);
+    Task<string?> GetChartJsonByIdAsync(int chartId);
+    Task<Chart?> GetChartWeakByIdAsync(int chartId);
+    Task<Branch?> GetBranchWeakByIdAsync(int branchId);
+    Task<Station?> GetStationWeakByIdAsync(int stationId);
+    Task<Railway?> GetRailwayByIdAsync(int railwayId);
+    Task<Transition?> GetTransitionByIdAsync(int transitionId);
 
     Task<int> GetChartIdAsync(string city, string title);
     Task<int> GetBranchIdAsync(string title, int chartId);
     Task<int> GetStationIdAsync(string title, int branchId);
 
-    Task<string> GetChartJsonAsync(string city, string title);
+    Task<string?> GetChartJsonAsync(string city, string title);
 
     Task<List<int>> GetAllChartIdAsync();
     Task<List<int>> GetAllChartBranchIdAsync(int chartId);
@@ -33,13 +33,13 @@ public interface IChartRepository
     Task<List<int>> GetFromToStationIdByRailwayIdAsync(int railwayId);
     Task<List<int>> GetFromToStationIdByTransitionIdAsync(int transitionId);
 
-    Task UpdateChartByIdAsync(int chartId, Chart chart);
-    Task UpdateBranchByIdAsync(int branchId, Branch branch);
-    Task UpdateStationByIdAsync(int stationId, Station station);
-    Task UpdateRailwayByIdAsync(int railwayId, Railway railway);
-    Task UpdateTransitionByIdAsync(int stationId, Transition transition);
+    Task<int> UpdateChartByIdAsync(int chartId, Chart chart);
+    Task<int> UpdateBranchByIdAsync(int branchId, Branch branch);
+    Task<int> UpdateStationByIdAsync(int stationId, Station station);
+    Task<int> UpdateRailwayByIdAsync(int railwayId, Railway railway);
+    Task<int> UpdateTransitionByIdAsync(int stationId, Transition transition);
 
-    Task DeleteChartByIdAsync(int id);
+    Task<int> DeleteChartByIdAsync(int id);
 
     Task<bool> IsCityTitleUniqueAsync(string city, string title);
     Task<bool> IsBranchTitleUniqueInChartAsync(string title, int chartId);
--- MetroGid/Core/Interfaces/IRouteRepository.cs
+++ MetroGid/Core/Interfaces/IRouteRepository.cs
@@ -7,15 +7,15 @@ public interface IRouteRepository
     Task<int> AddAsync(Route route, int clientId, int chartId);
 
     Task<int> GetIdAsync(string title, int clientId);
-    Task<Route> GetByIdAsync(int id);
+    Task<Route?> GetByIdAsync(int id);
     Task<List<string>> GetAllTitlesForClientAsync(int clientId);
     Task<List<string>> GetAllTitlesForClientOfChartAsync(int clientId, int chartId);
     Task<List<Route>> GetAllForClientIdAsync(int clientId);
     Task<List<Route>> GetAllForClientOfChartIdAsync(int clientId, int chartId);
 
-    Task UpdateAsync(int id, Route route);
+    Task<int> UpdateAsync(int id, Route route);
 
-    Task DeleteAsync(int id);
+    Task<int> DeleteAsync(int id);
 
     Task<bool> IsTitleExistsAsync(string title, int clientId);
 }
