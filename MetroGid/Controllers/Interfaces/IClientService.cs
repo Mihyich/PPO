18246dec9cd1901eebf28f942245a8644465e96d
GrizlyBear <michail.zevahin@gmail.com>
2025-08-19 10:21:23 +0300

    Скорректированы типы возвращаемых переменных

--- MetroGid/Controllers/Interfaces/IClientService.cs
+++ MetroGid/Controllers/Interfaces/IClientService.cs
@@ -7,11 +7,11 @@ public interface IClientService
     // Создание пользователя в БД
     Task<int> Reg(string login, string password, string mail);
     // Удаление пользователя из БД
-    Task UnReg(string login, string password, string mail);
+    Task<int> UnReg(string login, string password, string mail);
     // Вход
     Task<RoleTypeDTO> SignIn(string login, string password, string mail);
     // Выход
-    Task SignOut(string login, string password, string mail);
+    Task<int> SignOut(string login, string password, string mail);
     // Получить роль пользователя
     Task<RoleTypeDTO> GetRole(string login, string password, string mail);
 }
