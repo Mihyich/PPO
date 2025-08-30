f974087443d6567d02e71a4f61b40d39dd2c4ccd
GrizlyBear <michail.zevahin@gmail.com>
2025-08-30 16:08:05 +0300

    Обновлен контроллер маршрутов

--- MetroGid/Controllers/Unsigned/RouteController.cs
+++ MetroGid/Controllers/Unsigned/RouteController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using MetroGid.Controllers.Attributes;
 using MetroGid.Controllers.Utility.DTO.Concrete;
 using MetroGid.Controllers.Utility.DTO.Route;
@@ -12,10 +13,12 @@ namespace MetroGid.Controllers.Unsigned;
 [ApiController]
 [Route("api/route")]
 public class RouteController(
-    IRouteService routeService
+    IRouteService routeService,
+    IChartService chartService
 ) : ControllerBase
 {
     private readonly IRouteService _routeService = routeService;
+    private readonly IChartService _chartService = chartService;
 
     [AllowAnonymous]
     [HttpPost("search")]
@@ -29,7 +32,89 @@ public class RouteController(
             )
         );
 
-    // [RequireAnyRole(RoleTypeDTO.SIGNED, RoleTypeDTO.DUTY)]
-    // [HttpPost("Save")]
-    // public async Task<IActionResult> Save([FromBody] )
+    [RequireAnyRole(RoleTypeDTO.SIGNED, RoleTypeDTO.DUTY)]
+    [HttpPost("Save")]
+    public async Task<IActionResult> Save([FromBody] RouteDTO dto)
+    {
+        string? clientIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (!int.TryParse(clientIdClaim, out int clientId))
+            return StatusCode(
+                StatusCodes.Status401Unauthorized,
+                new
+                {
+                    Error = "InvalidCredentials",
+                    Message = "Не удалось определить пользователя"
+                }
+            );
+
+        int chartId = await _chartService.GetChartIdAsync(
+            dto.City,
+            dto.ChartTitle
+        );
+
+        return Ok(
+            await _routeService.SaveRouteAsync(
+                clientId,
+                chartId,
+                DtoRouteJsonConverter.Convert(dto)
+            )
+        );
+    }
+
+    [RequireAnyRole(RoleTypeDTO.SIGNED, RoleTypeDTO.DUTY)]
+    [HttpPost("route_titles")]
+    public async Task<IActionResult> GetRouteTitles([FromBody] GetRouteCredentialsRequest dto)
+    {
+        string? clientIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (!int.TryParse(clientIdClaim, out int clientId))
+            return StatusCode(
+                StatusCodes.Status401Unauthorized,
+                new
+                {
+                    Error = "InvalidCredentials",
+                    Message = "Не удалось определить пользователя"
+                }
+            );
+
+        int chartId = await _chartService.GetChartIdAsync(
+            dto.CityTitle,
+            dto.ChartTitle
+        );
+
+        return Ok(
+            await _routeService.GetSavedChartRoutesTitles(
+                clientId,
+                chartId
+            )
+        );
+    }
+
+    [RequireAnyRole(RoleTypeDTO.SIGNED, RoleTypeDTO.DUTY)]
+    [HttpPost("get")]
+    public async Task<IActionResult> GetSavedRoute([FromBody] GetSavedRouteRequest dto)
+    {
+        string? clientIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (!int.TryParse(clientIdClaim, out int clientId))
+            return StatusCode(
+                StatusCodes.Status401Unauthorized,
+                new
+                {
+                    Error = "InvalidCredentials",
+                    Message = "Не удалось определить пользователя"
+                }
+            );
+
+        int chartId = await _chartService.GetChartIdAsync(
+            dto.CityTitle,
+            dto.ChartTitle
+        );
+
+        return Ok(
+            await _routeService.GetSavedChart(
+                clientId,
+                chartId,
+                dto.RouteTitle
+            )
+        );
+    }
 }
